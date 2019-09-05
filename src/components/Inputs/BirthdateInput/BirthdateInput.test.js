423d7afe8814640405cf087b096b2e09cbf3e2f2
Rob Levin <rob@getethos.com>
2019-09-05 10:16:47 -0700

    Turns out you have to jest.mock react-text-mask to make snapshots work. It's also over in the monorepo fwiw.

--- src/components/Inputs/BirthdateInput/BirthdateInput.test.js
+++ src/components/Inputs/BirthdateInput/BirthdateInput.test.js
@@ -2,15 +2,26 @@ import React from 'react';
 import { BirthdateInput } from './BirthdateInput';
 import renderer from 'react-test-renderer';
 
+// TODO: make a helper
+// https://github.com/text-mask/text-mask/issues/427
+jest.mock('react-text-mask', () => (props) => <input type="text" {...props} />)
+
 describe('BirthdateInput', () => {
   it('default rendering', () => {
     const tree = renderer
-      .create(<BirthdateInput
-        name="le-birthdate"
-        labelCopy="What is your birthdate?"
-        data-tid='le-birthdate'
-        validator={(errors) => {}}
-      />).toJSON();
+      .create(
+        <BirthdateInput
+          dateFormat='mm/dd/yyyy'
+          name="le-birthdate"
+          labelCopy="What is your birthdate?"
+          data-tid='le-birthdate'
+          validator={(thing) => {}}
+          name="birthDate"
+          minAge={20}
+          maxAge={65}
+        />
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
