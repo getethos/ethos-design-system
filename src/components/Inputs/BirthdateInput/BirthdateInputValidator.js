5d275deac33b39785af0d71eddd09067817272d2
Rob Levin <rob@getethos.com>
2019-09-05 19:35:38 -0700

    BirthdateInput validation and parsing

--- src/components/Inputs/BirthdateInput/BirthdateInputValidator.js
+++ src/components/Inputs/BirthdateInput/BirthdateInputValidator.js
@@ -1,34 +1,106 @@
 import React from 'react'
+import dayjs from '../../../helpers/getDayjs.js'
 
-export const getMaxDateValidator = ({
-  maxDate,
-  customErrorMessage,
-  dateFormat = 'mm/dd/yyyy',
-}) => (value) => {
-  // Should pass if there is no value
-  // if (value == null || value === '') return undefined
-  // const m = moment(maxDate)
-  // const momentDateFormat = DATE_FORMAT_TO_MOMENT_FORMAT[dateFormat]
-  // const errorMessage =
-  //   customErrorMessage ||
-  //   `Please enter a date before ${m.format(momentDateFormat)}`
-  // return moment(value).isBefore(m) ? undefined : errorMessage
-  return undefined
+export const DATE_FORMATS = [
+  'mm/dd/yyyy',
+  'mm/yyyy',
+  'mm/yy',
+]
+
+export const dateMaskByFormat = {
+  'mm/dd/yyyy': [/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/],
+  'mm/yyyy': [/\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/],
+  'mm/yy': [/\d/, /\d/, '/', /\d/, /\d/],
 }
 
-export const getMinDateValidator = ({
-  minDate,
-  customErrorMessage,
-  dateFormat = 'mm/dd/yyyy',
-}) => (value) => {
-  // Should pass if there is no value
-  // if (value == null || value === '') return undefined
+export const dateRegexByFormat = {
+  'mm/dd/yyyy': /\d\d\/\d\d\/\d\d\d\d/,
+  'mm/yyyy': /\d\d\/\d\d\d\d/,
+  'mm/yy': /\d\d\/\d\d/,
+}
 
-  // const m = moment(minDate)
-  // const momentDateFormat = DATE_FORMAT_TO_MOMENT_FORMAT[dateFormat]
-  // const errorMessage =
-  //   customErrorMessage ||
-  //   `Please enter a date after ${m.format(momentDateFormat)}`
-  // return moment(value).isAfter(m) ? undefined : errorMessage
-  return undefined
+// first replace removes text-mask's underscores, second fixes like: '2//'
+export const cleanse = (value) => value.replace(/[_]/g,'').replace('//', '/')
+
+export const dateStringMatchesFormat = (cleansedDateString, dateFormat) => {
+  const pattern = dateRegexByFormat[dateFormat];
+  const matchesFormat = pattern.test(cleansedDateString);
+  console.log("matchesFormat: ", matchesFormat)
+  if (!matchesFormat) {
+    return 'Please enter a valid date.'
+  }
+  return '';
+}
+
+export const getMaxDateValidator = (props) => {
+  const {
+    maxDate,
+    customErrorMessage,
+    dateFormat,
+  } = props
+
+  return (value) => {
+    if (value == null || value === '') return ''
+    const date = dayjs(maxDate);
+    const dayjsFormat = dateFormat.toUpperCase();
+    const errorMessage =
+      customErrorMessage ||
+      `Please enter a date before ${date.format(dayjsFormat)}`
+    return dayjs(value).isBefore(date) ? '' : errorMessage
+  }
+}
+
+export const getMinDateValidator = (props) => {
+  const {
+    minDate,
+    customErrorMessage,
+    dateFormat,
+  } = props
+
+  return (value) => {
+    // Should pass if there is no value
+    if (value == null || value === '') return ''
+    const date = dayjs(minDate);
+    const dayjsFormat = dateFormat.toUpperCase();
+    const errorMessage =
+      customErrorMessage ||
+      `Please enter a date before ${date.format(dayjsFormat)}`
+    return dayjs(value).isAfter(date) ? '' : errorMessage
+  }
+}
+
+export const getMinMaxDateValidator = (props) => {
+  const {
+    minAge,
+    maxAge,
+    dateFormat,
+    customErrorMessage,
+  } = props;
+
+  return (value) => {
+    const dateRangeErrorMessage = customErrorMessage || `Sorry, you must be ${minAge}–${maxAge}.`
+    const minBirthdate = dayjs()
+      .subtract(maxAge + 1, 'years')
+      .endOf('day')
+      .toDate()
+
+    const maxBirthdate = dayjs()
+      .subtract(minAge, 'years')
+      .endOf('day')
+      .toDate()
+
+    const maxError = getMaxDateValidator({
+      maxDate: maxBirthdate,
+      customErrorMessage: dateRangeErrorMessage,
+      dateFormat,
+    })(value);
+
+    const minError = getMinDateValidator({
+      minDate: minBirthdate,
+      customErrorMessage: dateRangeErrorMessage,
+      dateFormat,
+    })(value);
+
+    return minError.length || maxError.length ? dateRangeErrorMessage : '';
+  }
 }
