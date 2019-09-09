d937e82c941efef7363e86a246fd9657cfcdebfd
Brad Walker <brad@getethos.com>
2019-09-09 16:07:37 -0700

    Fixing things, updating docs

--- src/components/ButtonSelectGroup/ButtonSelectGroup.js
+++ src/components/ButtonSelectGroup/ButtonSelectGroup.js
@@ -1,21 +1,44 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import uuidv4 from 'uuid/v4'
-import { OptionGroup } from './OptionGroup'
 import { OptionButton } from './OptionButton'
 import { Body } from '../Type/Body'
 import { COLORS } from '../Colors'
 import './ButtonGroup.scss'
 
 /**
- * @param {string} props.label - The button group's label
+ **/
+
+/**
+ * Component renders a group of button that behaves similarly to a radio group
+ *
+ * @public
+ *
+ * @param {object} props - Component Props
+ * @prop {string} props.label - Set's the caption of the group's label
+ * @prop {boolean} [props.allCaps=false] - When set to `true`, the group's label will be displayed in all caps
+ * @prop {string} [props.defaultValue] - Optionally sets a default value for the group. If set, the matching option will be set as `isSelected`
+ * @prop {function} [props.onSelect] - Optional callback thats fires when an option is selected. returns an object containing the selected `value` and a boolean value `isAnswered`
+ *
+ * @example ```
+ * <ButtonSelectGroup
+ *  defaultValue="excellent"
+ *  label="Health"
+ *  onSelect={({ value }) => console.log(value)}
+ * >
+ *   <ButtonSelectGroup.Option value="average">Average</ButtonSelectGroup.Option>
+ *   <ButtonSelectGroup.Option value="great">Greate</ButtonSelectGroup.Option>
+ * </ButtonSelectGroup>
+ * ```
+ *
+ * @return {JSX.Element}
  */
 export const ButtonSelectGroup = ({
   label,
-  allCaps,
+  allCaps = false,
   children,
-  onSelect,
   defaultValue,
+  onSelect,
 }) => {
   const [selectedValue, setSelectedValue] = useState(defaultValue)
   const [isAnswered, setIsAnswered] = useState(false)
@@ -26,15 +49,39 @@ export const ButtonSelectGroup = ({
     }
   }, [selectedValue])
 
-  function onSelectHandler(value) {
-    // set the selected value to the passed value
-    setSelectedValue(value)
+  /**
+   * Handles a click event on an descending `<OptionButton/>`
+   *
+   * @private
+   *
+   * @param {string} value - the new selected value
+   * @param {function} clickHandler - an optional click handler that fires are the value has been set
+   *
+   * @return {function}
+   */
+  function onClickHandler(value, clickHandler) {
+    return (evt) => {
+      // set the selected value to the passed value
+      setSelectedValue(value)
 
-    // if the group was not previously answered – set it to true
-    if (!isAnswered) {
-      setIsAnswered(true)
+      // if the group was not previously answered – set it to true
+      if (!isAnswered) {
+        setIsAnswered(true)
+      }
+
+      return clickHandler && clickHandler(evt)
     }
   }
+
+  // Iterate through each child, inject stateful props
+  const options = React.Children.map(children, (child) => {
+    const { value, onClick: passedHandler } = child.props
+
+    const isSelected = value === selectedValue
+    const onClick = onClickHandler(value, passedHandler)
+
+    return React.cloneElement(child, { ...child.props, isSelected, onClick })
+  })
 
   const labelId = `button-select-group-${uuidv4()}`
   return (
@@ -52,25 +99,21 @@ export const ButtonSelectGroup = ({
           {label}
         </span>
       </Body.Regular400>
-      <OptionGroup
-        selectedValue={selectedValue}
-        onSelectHandler={onSelectHandler}
-      >
-        {children}
-      </OptionGroup>
+      <div className="button-group">{options}</div>
     </div>
   )
 }
 
 ButtonSelectGroup.propTypes = {
-  /** Set's the caption of the label for the group */
-  label: PropTypes.string,
+  /** Set's the caption of the group's label */
+  label: PropTypes.string.isRequired,
   /** When set to `true`, the group's label will be displayed uppercase */
   allCaps: PropTypes.bool,
   /** Optional callback thats fires when an option is selected. returns an object containing the selected `value` and a boolean value `isAnswered` */
   onSelect: PropTypes.func,
-  /** Optionally sets a default value for the group. If set, the matching option will be set as `isSelected */
+  /** Optionally sets a default value for the group. If set, the matching option will be set as `isSelected` */
   defaultValue: PropTypes.string,
 }
 
+// Export the option button
 ButtonSelectGroup.Option = OptionButton
--- src/components/ButtonSelectGroup/ButtonSelectGroup.md
+++ src/components/ButtonSelectGroup/ButtonSelectGroup.md
@@ -1,13 +1,13 @@
-## Description 
+### Description
+`<ButtonSelectGroup />` renders a group of button that behaves similarly to a radio group
 
-## Examples
+
+### Examples
 
 ### Base Usage
 
 ```jsx
-<ButtonSelectGroup
-  label="Health"
->
+<ButtonSelectGroup label="Health">
   <ButtonSelectGroup.Option value="average">Average</ButtonSelectGroup.Option>
   <ButtonSelectGroup.Option value="great">Great</ButtonSelectGroup.Option>
   <ButtonSelectGroup.Option value="excellent">
@@ -16,13 +16,10 @@
 </ButtonSelectGroup>
 ```
 
-### Rendering with a `defaultValue`
+### Rendering with a `defaultValue` selected
 
 ```jsx
-<ButtonSelectGroup
-  defaultValue="great"
-  label="Health"
->
+<ButtonSelectGroup defaultValue="great" label="Health">
   <ButtonSelectGroup.Option value="average">Average</ButtonSelectGroup.Option>
   <ButtonSelectGroup.Option value="great">Great</ButtonSelectGroup.Option>
   <ButtonSelectGroup.Option value="excellent">
@@ -31,15 +28,32 @@
 </ButtonSelectGroup>
 ```
 
-### Handling `onSelect` 
+### Handling `onSelect`
 
 ```jsx
 <ButtonSelectGroup
-  defaultValue="great"
+  defaultValue="excellent"
   label="Health"
   onSelect={({ value }) => console.log(value)}
 >
   <ButtonSelectGroup.Option value="average">Average</ButtonSelectGroup.Option>
+  <ButtonSelectGroup.Option value="great">Great</ButtonSelectGroup.Option>
+  <ButtonSelectGroup.Option value="excellent">
+    Exellent
+  </ButtonSelectGroup.Option>
+</ButtonSelectGroup>
+```
+
+
+### Handling an `onClick` event on a specific `<Option/>` 
+
+```jsx
+<ButtonSelectGroup
+  defaultValue="excellent"
+  label="Health"
+  onSelect={({ value }) => console.log(value)}
+>
+  <ButtonSelectGroup.Option onClick={() => console.log('i was clicked!')} value="average">Average</ButtonSelectGroup.Option>
   <ButtonSelectGroup.Option value="great">Great</ButtonSelectGroup.Option>
   <ButtonSelectGroup.Option value="excellent">
     Exellent
