08d86141280964948cfb383763333a2685329841
Rob Levin <rob@getethos.com>
2019-10-01 07:26:45 -0700

    Setting up the text-mask for zip code. WIP

--- src/components/Inputs/ZipInput/ZipInput.js
+++ src/components/Inputs/ZipInput/ZipInput.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { TextInput } from '../TextInput';
+import MaskedInput from 'react-text-mask'
+import { InputLabel } from '../InputLabel'
 
 export const ZipInput = (props) => {
+  const {
+    name,
+    labelCopy,
+    allCaps,
+    validator,
+    // formChangeHandler,
+    ...restProps
+  } = props
+
   return (
-    <TextInput {...props} />
+    <>
+      <InputLabel name={name} labelCopy={labelCopy} allCaps={allCaps} />
+      <MaskedInput
+        mask={[/\d/, /\d/, /\d/, /\d/, /\d/]}
+        type="text"
+        inputmode="numeric"
+        data-tid={restProps['data-tid']}
+        guide={true}
+        // onBlur={onBlur}
+        // onChange={onChange}
+        name={props.name}
+        className={'ZipInput TextInput'}
+        keepCharPositions={true}
+      />
+    </>
   )
-};
+}
 
 ZipInput.PUBLIC_PROPS = {
   'data-tid': PropTypes.string.isRequired,
@@ -14,7 +38,7 @@ ZipInput.PUBLIC_PROPS = {
   allCaps: PropTypes.bool,
   name: PropTypes.string.isRequired,
   labelCopy: PropTypes.string.isRequired,
-  validator: PropTypes.func
+  validator: PropTypes.func,
 }
 
 ZipInput.propTypes = {
--- src/components/Inputs/ZipInput/ZipInputValidator.js
+++ src/components/Inputs/ZipInput/ZipInputValidator.js
@@ -2,14 +2,12 @@ import React from 'react'
 
 // Currently, we only support USA. When we start to support
 // other locales, refer to http://html5pattern.com/Postal_Codes.
-const validZipRegex = /^(\d{5}([\-]\d{4})?)$/;
+const validZipRegex = /^(\d{5})$/
 
 const ZIP_CODE_INVALID_ERROR_MESSAGE = 'Please enter a valid Zip Code.'
 
 const ZipInputValidator = (postal) => {
-  return validZipRegex.test(postal)
-    ? ''
-    : ZIP_CODE_INVALID_ERROR_MESSAGE
+  return validZipRegex.test(postal) ? '' : ZIP_CODE_INVALID_ERROR_MESSAGE
 }
 
 export default ZipInputValidator
