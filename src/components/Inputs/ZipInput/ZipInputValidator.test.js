37dc7951932ca9aa7aab5376cbfc83f3ce6375cb
Rob Levin <rob@getethos.com>
2019-10-01 11:24:25 -0700

    Update the ZipInput tests. Need to use the jest mock for react-text-mask since we're now using text-mask

--- src/components/Inputs/ZipInput/ZipInputValidator.test.js
+++ src/components/Inputs/ZipInput/ZipInputValidator.test.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import zipInputValidator from './ZipInputValidator.js';
+import React from 'react'
+import zipInputValidator from './ZipInputValidator.js'
 
 describe('ZipInputValidator', () => {
   it('validates 5 digit zip codes', () => {
@@ -8,14 +8,5 @@ describe('ZipInputValidator', () => {
     expect(zipInputValidator('945').length === 0).toBeFalsy()
     expect(zipInputValidator('9454').length === 0).toBeFalsy()
     expect(zipInputValidator('94546').length === 0).toBeTruthy()
-  });
-
-  // hyphenated part e.g. 94546-1234
-  it('validates 5 digit, hyphen, 4 digits', () => {
-    expect(zipInputValidator('94546-').length === 0).toBeFalsy()
-    expect(zipInputValidator('94546-1').length === 0).toBeFalsy()
-    expect(zipInputValidator('94546-12').length === 0).toBeFalsy()
-    expect(zipInputValidator('94546-123').length === 0).toBeFalsy()
-    expect(zipInputValidator('94546-1234').length === 0).toBeTruthy()
-  });
-});
+  })
+})
