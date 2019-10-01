fd89ffbb5862599b3d37b827eaff172a37220275
Cole <c7hudson@gmail.com>
2019-10-01 11:49:24 -0700

    crop parameter configuration and progressive image format flag

--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -8,7 +8,7 @@ export const CLOUDINARY_CLOUD_NAME = 'getethos'
 
 const publicIdBase = `https://res.cloudinary.com/${CLOUDINARY_CLOUD_NAME}/image/upload/`
 
-export const CloudinaryImage = ({ publicId, className, ...rest }) => {
+export const CloudinaryImage = ({ publicId, className, crop, ...rest }) => {
   const publicIdFilename = publicId.replace(publicIdBase, '')
   return (
     <Cloudinary.Image
@@ -18,14 +18,47 @@ export const CloudinaryImage = ({ publicId, className, ...rest }) => {
       dpr="auto"
       responsive
       publicId={publicIdFilename}
-      crop="fill" // aka CSS cover
+      crop={crop} 
       secure="true"
+      flags="progressive:semi"
     />
   )
+}
+
+/**
+* https://cloudinary.com/documentation/image_transformation_reference#crop_parameter
+* 
+* fill: Create an image with the exact given width and height without distorting the 
+*       image. This option first scales as much as needed to at least fill both of the 
+*       given dimensions. If the requested aspect ratio is different than the original, 
+*       cropping will occur on the dimension that exceeds the requested size after scaling.
+*       Similar to CSS background-size "cover".
+*
+* fit: The image is resized so that it takes up as much space as possible within 
+*      a bounding box defined by the given width and height parameters. The original 
+*      aspect ratio is retained and all of the original image is visible.
+*      Similar to CSS background-size "contain".
+*
+* crop: Used to extract a given width & height out of the original image. The original 
+*       proportions are retained and so is the size of the graphics.
+*       Similar to CSS background-size "auto".
+*
+*
+**/
+CloudinaryImage.CROP_METHODS = {
+  FILL: 'fill',
+  FIT: 'fit',
+  CROP: 'crop',
+
 }
 
 CloudinaryImage.propTypes = {
   // We're trying to avoid/reduce className restyling, but let's try it here:
   className: PropTypes.string,
   publicId: PropTypes.string.isRequired,
+  crop: PropTypes.oneOf(Object.values(CloudinaryImage.CROP_METHODS)),
+}
+
+CloudinaryImage.defaultProps = {
+  crop: 'fill',
 }
