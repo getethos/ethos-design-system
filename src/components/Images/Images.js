9fe4c15e0bff9fe1a25e9b839f601e984e236728
Nick Mcco <hwerzog@gmail.com>
2019-10-01 21:56:34 -0700

    Update to <Image>

--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -1,64 +1,114 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import * as Cloudinary from 'cloudinary-react'
+import cloudinary from 'cloudinary-core'
+import useRequired from '../../hooks/useRequired.js'
+import useInvalid from '../../hooks/useInvalid.js'
 
 // https://cloudinary.com/documentation/image_transformation_reference
 
 export const CLOUDINARY_CLOUD_NAME = 'getethos'
 
-const publicIdBase = `https://res.cloudinary.com/${CLOUDINARY_CLOUD_NAME}/image/upload/`
+const cld = new cloudinary.Cloudinary({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+})
 
-export const CloudinaryImage = ({ publicId, className, crop, ...rest }) => {
-  const publicIdFilename = publicId.replace(publicIdBase, '')
+const BREAKPOINTS = [600, 900, 1200]
+
+export const CloudinaryImage = ({
+  publicId,
+  className,
+  alt,
+  width,
+  height,
+  crop,
+  ...rest
+}) => {
+  // Verify that all required props were supplied
+  const [includesRequired] = useRequired(['publicId', 'alt'])
+  let allRelevantProps = Object.assign({}, rest, {
+    publicId,
+    alt,
+  })
+  includesRequired(allRelevantProps)
+
+  // Verify that no invalid props were supplied
+  const [includesInvalid] = useInvalid(
+    Object.keys(CloudinaryImage.PUBLIC_PROPS)
+  )
+  includesInvalid(rest)
+
+  const srcSetOptions = Object.assign(
+    {},
+    {
+      // Find information on image transformations here:
+      // https://cloudinary.com/documentation/image_transformations
+      quality: 'auto',
+      fetchFormat: 'auto',
+      srcset: {
+        breakpoints: BREAKPOINTS,
+        sizes: true,
+      },
+      dpr: 'auto',
+      crop,
+      secure: 'true', // ?
+      detection: 'adv_face', // https://cloudinary.com/documentation/advanced_facial_attributes_detection_addon
+    },
+    !!width ? { width } : {},
+    !!height ? { height } : {}
+  )
+
+  const srcSet = cld.imageTag(publicId, srcSetOptions).getAttr('srcset')
+
   return (
-    <Cloudinary.Image
+    <img
+      className={['Image lazyload', className].join(' ')}
+      src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+      data-src=""
+      data-sizes="auto"
+      srcSet={srcSet}
+      alt={alt}
       {...rest}
-      className={['Image', className].join(' ')}
-      cloudName={CLOUDINARY_CLOUD_NAME}
-      dpr="auto"
-      responsive
-      publicId={publicIdFilename}
-      crop={crop} 
-      secure="true"
-      flags="progressive:semi"
     />
   )
 }
 
 /**
-* https://cloudinary.com/documentation/image_transformation_reference#crop_parameter
-* 
-* fill: Create an image with the exact given width and height without distorting the 
-*       image. This option first scales as much as needed to at least fill both of the 
-*       given dimensions. If the requested aspect ratio is different than the original, 
-*       cropping will occur on the dimension that exceeds the requested size after scaling.
-*       Similar to CSS background-size "cover".
-*
-* fit: The image is resized so that it takes up as much space as possible within 
-*      a bounding box defined by the given width and height parameters. The original 
-*      aspect ratio is retained and all of the original image is visible.
-*      Similar to CSS background-size "contain".
-*
-* crop: Used to extract a given width & height out of the original image. The original 
-*       proportions are retained and so is the size of the graphics.
-*       Similar to CSS background-size "auto".
-*
-*
-**/
+ * https://cloudinary.com/documentation/image_transformation_reference#crop_parameter
+ *
+ * fill: Create an image with the exact given width and height without distorting the
+ *       image. This option first scales as much as needed to at least fill both of the
+ *       given dimensions. If the requested aspect ratio is different than the original,
+ *       cropping will occur on the dimension that exceeds the requested size after scaling.
+ *       Similar to CSS background-size "cover".
+ *
+ * fit: The image is resized so that it takes up as much space as possible within
+ *      a bounding box defined by the given width and height parameters. The original
+ *      aspect ratio is retained and all of the original image is visible.
+ *      Similar to CSS background-size "contain".
+ *
+ * crop: Used to extract a given width & height out of the original image. The original
+ *       proportions are retained and so is the size of the graphics.
+ *       Similar to CSS background-size "auto".
+ *
+ *
+ **/
 CloudinaryImage.CROP_METHODS = {
   FILL: 'fill',
   FIT: 'fit',
   CROP: 'crop',
-
 }
 
-CloudinaryImage.propTypes = {
-  // We're trying to avoid/reduce className restyling, but let's try it here:
+CloudinaryImage.PUBLIC_PROPS = {
+  height: PropTypes.number,
+  width: PropTypes.number,
   className: PropTypes.string,
+  alt: PropTypes.string.isRequired,
   publicId: PropTypes.string.isRequired,
   crop: PropTypes.oneOf(Object.values(CloudinaryImage.CROP_METHODS)),
 }
 
 CloudinaryImage.defaultProps = {
-  crop: 'fill',
+  crop: CloudinaryImage.CROP_METHODS.FILL,
 }
+
+CloudinaryImage.propTypes = CloudinaryImage.PUBLIC_PROPS
