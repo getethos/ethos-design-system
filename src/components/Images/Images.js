63ac207e2e07a197def27e3d9315ac242764ed7c
Nick Mcco <hwerzog@gmail.com>
2019-10-01 17:18:22 -0700

    First shot at new <Image>

--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -1,31 +1,81 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import * as Cloudinary from 'cloudinary-react'
+import cloudinary from 'cloudinary-core'
+import useRequired from '../../hooks/useRequired.js'
+import useInvalid from '../../hooks/useInvalid.js'
 
 // https://cloudinary.com/documentation/image_transformation_reference
 
 export const CLOUDINARY_CLOUD_NAME = 'getethos'
 
-const publicIdBase = `https://res.cloudinary.com/${CLOUDINARY_CLOUD_NAME}/image/upload/`
+const cld = new cloudinary.Cloudinary({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+})
 
-export const CloudinaryImage = ({ publicId, className, ...rest }) => {
-  const publicIdFilename = publicId.replace(publicIdBase, '')
+const BREAKPOINTS = [600, 900, 1200]
+
+export const CloudinaryImage = ({
+  publicId,
+  className,
+  alt,
+  width,
+  height,
+  ...rest
+}) => {
+  // Verify that all required props were supplied
+  const [includesRequired] = useRequired(['publicId', 'alt'])
+  let allRelevantProps = Object.assign({}, rest, {
+    publicId,
+    alt,
+  })
+  includesRequired(allRelevantProps)
+
+  // Verify that no invalid props were supplied
+  const [includesInvalid] = useInvalid(
+    Object.keys(CloudinaryImage.PUBLIC_PROPS)
+  )
+  includesInvalid(rest)
+
+  const srcSetOptions = Object.assign(
+    {},
+    {
+      // Find information on image transformations here:
+      // https://cloudinary.com/documentation/image_transformations
+      quality: 'auto',
+      fetchFormat: 'auto',
+      srcset: {
+        breakpoints: [600, 900, 1200],
+        sizes: true,
+      },
+      dpr: 'auto',
+      crop: 'fill', // ?
+      secure: 'true', // ?
+    },
+    !!width ? { width } : {},
+    !!height ? { height } : {}
+  )
+
+  const srcSet = cld.imageTag(publicId, srcSetOptions).getAttr('srcset')
+
   return (
-    <Cloudinary.Image
+    <img
+      className={['Image lazyload', className].join(' ')}
+      src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+      data-src=""
+      data-sizes="auto"
+      srcSet={srcSet}
+      alt={alt}
       {...rest}
-      className={['Image', className].join(' ')}
-      cloudName={CLOUDINARY_CLOUD_NAME}
-      dpr="auto"
-      responsive
-      publicId={publicIdFilename}
-      crop="fill" // aka CSS cover
-      secure="true"
     />
   )
 }
 
-CloudinaryImage.propTypes = {
-  // We're trying to avoid/reduce className restyling, but let's try it here:
+CloudinaryImage.PUBLIC_PROPS = {
+  height: PropTypes.number,
+  width: PropTypes.number,
   className: PropTypes.string,
+  alt: PropTypes.string.isRequired,
   publicId: PropTypes.string.isRequired,
 }
+
+CloudinaryImage.propTypes = CloudinaryImage.PUBLIC_PROPS
--- src/components/Images/Images.md
+++ src/components/Images/Images.md
@@ -1,3 +1,37 @@
 ```jsx
-<CloudinaryImage publicId="https://res.cloudinary.com/getethos/image/upload/v1565712179/01_NEW%20Lifestyle%20%28Rebrand%29/life-insurance-father-and-kids-playing.jpg" />
+<div style={{ width: 650 }}>
+  <ul>
+    <li>These images should lazy load.</li>
+    <li>
+      When viewed in a retina screen (like a macbook) this will load a high
+      resolution image.
+    </li>
+    <li>
+      You can test this image's responsiveness with chrome inspector using an
+      ipad, an iphone 6/7/8, and an iphone 5/SE; they should load different
+      images based on device resolution (although the specifics are determined
+      by ) and hard refreshing with different devices.
+    </li>
+  </ul>
+  <CloudinaryImage
+    width={650}
+    alt="father and kids playing"
+    publicId="v1565712179/01_NEW%20Lifestyle%20%28Rebrand%29/life-insurance-father-and-kids-playing.jpg"
+  />
+</div>
+
+<br/>
+<br/>
+
+<ul>
+  <li>Due to not being constrained by the `width` property, this image should be bigger. It should also load a very large, retina-resolution image in comparison to the above image.
+  </li>
+  <li>
+    Without getting passed in a "width" prop, this image will be higher resolution on all retina displays, despite sometimes displaying smaller. I think this is OK, although it seems off?
+  </li>
+</ul>
+<CloudinaryImage
+  alt="father and kids playing"
+  publicId="v1565712179/01_NEW%20Lifestyle%20%28Rebrand%29/life-insurance-father-and-kids-playing.jpg"
+/>
 ```
