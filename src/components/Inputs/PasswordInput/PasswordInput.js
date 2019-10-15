25a43562a330f6dad226178aa2df662919a583d4
Rob Levin <rob@getethos.com>
2019-10-15 06:46:39 -0700

    Adds a PasswordInput component.

---
+++ src/components/Inputs/PasswordInput/PasswordInput.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import { TextInput } from '../TextInput'
+
+export const PasswordInput = (props) => (
+  <TextInput
+    type="password"
+    restrictIllegal={false}
+    {...props}
+  />
+)
+
