694faaa5cc1580983c1af80caf1419745f89e9a6
James Hawkins <james@getethos.com>
2019-10-21 12:15:10 -0700

    Add source to EmailValidator Regex

--- src/validators/EmailValidator.js
+++ src/validators/EmailValidator.js
@@ -1,4 +1,5 @@
-const validEmailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+// Regex pulled from here: https://stackoverflow.com/a/46181
+const validEmailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 const EMAIL_FORMAT_ERROR_MESSAGE = 'Please enter a valid email.'
 
