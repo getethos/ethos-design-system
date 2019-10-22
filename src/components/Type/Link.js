a18d1c86dde91ba428c5cde020f933a716565845
Zach Alexander <zach@getethos.com>
2019-10-21 18:03:31 -0700

    Move Link to CSS modules

--- src/components/Type/Link.js
+++ src/components/Type/Link.js
@@ -1,12 +1,14 @@
 import { Type, TypeFoundry } from './Type.js'
 
+import styles from './Link.module.scss'
+
 // Links – WIP. For now let's just export CSS classes and be otherwise agnostic.
+
 export const Link = {
-  CLASS_NAME: 'Link',
   STYLE_VARIANTS: {
     INHERIT: 'Inherit',
     STANDARD: 'Standard',
     NAVLINK: 'Navlink',
-    HAMBURGER_MENU: 'HamburgerMenu',
   },
+  styles,
 }
