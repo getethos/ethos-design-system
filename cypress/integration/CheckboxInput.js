20cd0a7fead4bf141761d220d56ae8539e89c56b
Rob Levin <rob@getethos.com>
2019-10-27 16:40:14 -0700

    Some formatting

--- cypress/integration/CheckboxInput.js
+++ cypress/integration/CheckboxInput.js
@@ -4,16 +4,16 @@ describe('CheckboxInput', () => {
   })
 
   //TODO -- I'd think following would work, but cypress didn't cooperate :(
-  // .trigger('keyCode', { key: 'Space', keyCode: 32, force: true}) 
+  // .trigger('keyCode', { key: 'Space', keyCode: 32, force: true})
   it('toggles checked', () => {
     cy.get('[data-tid="le-tid-unchecked"]')
-      .check({force: true})
+      .check({ force: true })
       .should('be.checked')
   })
 
   it('toggles unchecked', () => {
     cy.get('[data-tid="le-tid-checked"]')
-      .uncheck({force: true})
+      .uncheck({ force: true })
       .should('not.be.checked')
   })
 })
