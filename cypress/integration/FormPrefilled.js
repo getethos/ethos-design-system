ee74aa9d532a3ca1d5cc1756dec50ad77bdd0736
Rob Levin <rob@getethos.com>
2019-10-28 15:28:34 -0500

    Hopefully sets up Travis CI to successfully run Cypress e2e integration specs
    Since we do not have fonts public, we need to bypass the require statement for them when in NODE_ENV=ci and this does that.

--- cypress/integration/FormPrefilled.js
+++ cypress/integration/FormPrefilled.js
@@ -1,17 +1,17 @@
 describe('FormPrefilled', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:9008/#/Components/FormPrefilled')
+    cy.visit('/#/Components/FormPrefilled')
   })
 
   it('fully prefilled form should result in submit button enabled', () => {
-    cy.get('[data-tid="button-prefilled-submit"]').should('not.be.disabled')
+    cy.getByTid('button-prefilled-submit').should('not.be.disabled')
   })
 
   it('any invalid field should result in submit button disabled', () => {
     const invalid = 'too long a string'
-    cy.get('[data-tid="even-num-text"]')
+    cy.getByTid('even-num-text')
       .type(invalid)
       .trigger('blur')
-    cy.get('[data-tid="button-prefilled-submit"]').should('be.disabled')
+    cy.getByTid('button-prefilled-submit').should('be.disabled')
   })
 })
