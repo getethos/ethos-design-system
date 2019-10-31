f975b4f9324f0b9b4f4f2cd1f5d19e5a675fdcd0
zach-ethos <49569137+zach-ethos@users.noreply.github.com>
2019-10-30 20:26:52 -0700

    Cap lint problems (#130)
    * Fail CI if additional lint issues are created
    
    Currently there are 82 warnings, about half `no-unused-vars`,
    about half `react/prop-types`.
    
    * Fix lint issues in Spacer test
    
    * Lower max warnings
    
    * Restore test renderer lost in recent changes

--- src/components/Spacer/Spacer.test.js
+++ src/components/Spacer/Spacer.test.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
 import { Spacer } from './Spacer.js'
 
 describe('Spacer', () => {
@@ -58,10 +60,6 @@ describe('Spacer', () => {
       expect(deepSnapshot(<Spacer.W64 />)).toMatchSnapshot()
     })
 
-    test('Spacer.W64', () => {
-      expect(deepSnapshot(<Spacer.W64 />)).toMatchSnapshot()
-    })
-
     test('Spacer.W56', () => {
       expect(deepSnapshot(<Spacer.W56 />)).toMatchSnapshot()
     })
@@ -92,35 +90,6 @@ describe('Spacer', () => {
 
     test('Spacer.W4', () => {
       expect(deepSnapshot(<Spacer.W4 />)).toMatchSnapshot()
-    })
-  })
-
-  describe('API', () => {
-    test('Spacer exports properly', () => {
-      expect(Spacer).toBeDefined()
-      expect(Spacer.H80).toBeDefined()
-      expect(Spacer.H72).toBeDefined()
-      expect(Spacer.H64).toBeDefined()
-      expect(Spacer.H56).toBeDefined()
-      expect(Spacer.H48).toBeDefined()
-      expect(Spacer.H40).toBeDefined()
-      expect(Spacer.H32).toBeDefined()
-      expect(Spacer.H24).toBeDefined()
-      expect(Spacer.H16).toBeDefined()
-      expect(Spacer.H8).toBeDefined()
-      expect(Spacer.H4).toBeDefined()
-      expect(Spacer.W80).toBeDefined()
-      expect(Spacer.W72).toBeDefined()
-      expect(Spacer.W64).toBeDefined()
-      expect(Spacer.W64).toBeDefined()
-      expect(Spacer.W56).toBeDefined()
-      expect(Spacer.W48).toBeDefined()
-      expect(Spacer.W40).toBeDefined()
-      expect(Spacer.W32).toBeDefined()
-      expect(Spacer.W24).toBeDefined()
-      expect(Spacer.W16).toBeDefined()
-      expect(Spacer.W8).toBeDefined()
-      expect(Spacer.W4).toBeDefined()
     })
   })
 })
