c64a3c5c048bba2768a8c376dc3cbb15b1141182
Rob Levin <rob@getethos.com>
2019-11-01 06:39:52 -0500

    Fixes warnings on children being an object

--- src/components/Inputs/ButtonSelectGroup/ButtonSelectGroup.js
+++ src/components/Inputs/ButtonSelectGroup/ButtonSelectGroup.js
@@ -149,7 +149,7 @@ export const ButtonSelectGroup = ({
 }
 
 ButtonSelectGroup.propTypes = {
-  children: PropTypes.arrayOf(PropTypes.instanceOf(OptionButton)).isRequired,
+  children: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
   /** Set's the caption of the group's label */
   labelCopy: PropTypes.string.isRequired,
   /** Name of the field, provided a uuid if not supplied. */
