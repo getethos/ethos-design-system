0f99c5a0cf1ab190d5ae3688bea5dc71ddae05fe
Rob Levin <roblevintennis@gmail.com>
2019-11-01 18:06:01 -0600

    Fix more issues found with PropTypes (#140)
    * Fix more issues found with PropTypes. Also, EmailInput wasn't passing through the disabled prop to TextInput
    
    * Destructure out offsetHeight so we do not have 'transferring props' to children. It was causing a prop types error.

--- src/components/Inputs/ButtonSelectGroup/ButtonSelectGroup.js
+++ src/components/Inputs/ButtonSelectGroup/ButtonSelectGroup.js
@@ -149,6 +149,9 @@ export const ButtonSelectGroup = ({
 }
 
 ButtonSelectGroup.propTypes = {
+  formTouched: PropTypes.bool,
+  currentValue: PropTypes.string,
+  currentError: PropTypes.string,
   children: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
   /** Set's the caption of the group's label */
   labelCopy: PropTypes.string.isRequired,
