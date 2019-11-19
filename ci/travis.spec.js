6fea81fc234298406b04774a6e1875af0ae9f180
Rob Levin <rob@getethos.com>
2019-11-19 06:48:53 -0800

    Attempts to combine patch and any additional information, and also evaluates comparison as string

--- ci/travis.spec.js
+++ ci/travis.spec.js
@@ -62,12 +62,10 @@ function versionAtRef(refName = BASE_BRANCH) {
 // combined to: "17-beta1" which can be compared to say "17-beta2" in
 // expect(prVersion.patch).toBeGreaterThan(baseVersion.patch)
 function versionNumbersAtRef() {
-  const [major, minor, patch, extra] = versionAtRef()
-    .split('.')
+  const [major, minor, patch, extra] = versionAtRef().split('.')
   let combinedPatch = patch
   if (patch && extra) {
     combinedPatch = `${patch}${extra}`
   }
   return { major, minor, combinedPatch }
- }
 }
