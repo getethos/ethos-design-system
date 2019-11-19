6dd9dec0275fb51f415558940961bbb9405577b5
Rob Levin <rob@getethos.com>
2019-11-18 18:44:46 -0800

    Docs tell me it's lower case so hopefully that will help CI/CD out

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import ReactSelect from 'react-select'
-import ReactSelectAsync from 'react-select/Async'
+import ReactSelectAsync from 'react-select/async'
 
 import styles from './Select.module.scss'
 
