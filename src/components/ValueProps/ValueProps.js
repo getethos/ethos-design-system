6a6931b3b707f6f127ea2b59dfb38fca351d4b63
Rob Levin <rob@getethos.com>
2019-11-20 17:02:25 -0800

    first stab at porting over the package select value props component

---
+++ src/components/ValueProps/ValueProps.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import uuidv4 from 'uuid/v4'
+import { TitleSmall, Body, CloudinaryImage } from '../index'
+
+import styles from './ValueProps.module.scss'
+
+export function ValueProps() {
+  return (
+    <div className={`${styles.container} ${styles.root}`}>
+      {sections.map((section) => (
+        <div className={styles.section} key={uuidv4()}>
+          <div>
+            <CloudinaryImage
+              publicId={section.iconUrl}
+              alt={section.alt}
+              width={[56, 56, 56, 56]}
+              height={[56, 56, 56, 56]}
+            />
+          </div>
+          <div className={styles.text}>
+            <TitleSmall.Sans.Regular400>
+              {section.header}
+            </TitleSmall.Sans.Regular400>
+            <Body.Regular400>{section.subHeader}</Body.Regular400>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const sections = [
+  {
+    iconUrl:
+      'https://res.cloudinary.com/getethos/image/upload/v1566238234/02_Icons/icon_hourglass_xedxvp.svg',
+    header: 'Fast, easy, free application',
+    subHeader:
+      'Our online application is straightforward and can be completed in minutes.',
+    alt: 'Hourglas Icon',
+  },
+  {
+    iconUrl:
+      'https://res.cloudinary.com/getethos/image/upload/v1566238232/02_Icons/icon_half-dollar_wsrssk.svg',
+    header: 'No cancellation fees',
+    subHeader:
+      "You can cancel your policy anytime, with no extra fees. If you change your mind within the first 30 days, we'll refund your payment in full.",
+  },
+  {
+    iconUrl:
+      'https://res.cloudinary.com/getethos/image/upload/v1566238234/02_Icons/icon_heart-shield_sh7goi.svg',
+    header: 'Trusted Partners',
+    subHeader:
+      'We partner with Legal & General America, RGAX, Assurity Life Insurance Company, and Munich Re to provide you affordable coverage.',
+    alt: 'Hear Shield Icon',
+  },
+]
+
+export default ValueProps
---
+++ src/components/ValueProps/index.js
@@ -0,0 +1,2 @@
+export { ValueProps } from './ValueProps'
+
