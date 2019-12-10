ec3c5492c80b6a6e11b4105a2d8894abc44eb672
Tamir Karssli <tkarssli@gmail.com>
2019-12-10 15:46:42 -0800

    Frequently Asked Questions (#181)
    * Migrate Faq to EDS
    
    * Fix proptyes
    
    * Create spec file
    
    * create snapshot
    
    * v1.1.34
    
    * Move to spriteloader
    
    * update snapshots
    
    * v1.1.35
    
    * v1.1.35
    
    * Add description
    
    * Revert "Move to spriteloader"
    
    This reverts commit 8bd6f1d19dabcbdb5f870e04cf3ac6960b860c09.
    
    * snapshots

---
+++ src/components/Faq/Faq.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import uuidv4 from 'uuid/v4'
+import { TitleLarge, TitleMedium, Body } from '../index'
+
+import styles from './Faq.module.scss'
+
+export const Faq = ({ questions, open }) => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.leftColumn}>
+        <TitleMedium.Serif.Book500>FAQs</TitleMedium.Serif.Book500>
+      </div>
+
+      <div className={styles.rightColumn}>
+        <div className={styles.phoneTitle}>
+          <TitleLarge.Serif.Book500>FAQs</TitleLarge.Serif.Book500>
+        </div>
+        {questions.map((q, index) => {
+          const startOpen = open && index === 0
+          return (
+            <Details summary={q.summary} open={startOpen} key={uuidv4()}>
+              {q.content}
+            </Details>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+Faq.defaultProps = {
+  questions: [],
+  open: true,
+}
+
+Faq.propTypes = {
+  questions: PropTypes.arrayOf(
+    PropTypes.shape({
+      summary: PropTypes.string,
+      content: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    })
+  ),
+  /** Set to false for all questions to start collapsed. */
+  open: PropTypes.bool,
+}
+
+const Details = ({ children, summary, open }) => {
+  const carrotSvg = (
+    <svg viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M0.999999 8.375L9.375 16.75L17.75 8.375"
+        stroke="black"
+        strokeOpacity="0.85"
+        strokeWidth="1.5"
+      />
+    </svg>
+  )
+
+  return (
+    <details open={open}>
+      <summary>
+        <div className={styles.summary}>
+          <Body.Medium500>{summary}</Body.Medium500>
+          <div className={styles.carrot}>{carrotSvg}</div>
+        </div>
+      </summary>
+      <div className={styles.body}>
+        <Body.Regular400>{children}</Body.Regular400>
+      </div>
+    </details>
+  )
+}
+
+Details.defaultProps = {
+  open: false,
+}
+
+Details.propTypes = {
+  children: PropTypes.node.isRequired,
+  summary: PropTypes.string.isRequired,
+  open: PropTypes.bool,
+}
+
+export default Faq
