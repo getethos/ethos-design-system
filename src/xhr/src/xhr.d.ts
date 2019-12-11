ca0296610bcdd37839d7f0c44eafe36c8cc4a7d7
Rob Levin <rob@getethos.com>
2019-12-11 06:32:01 -0800

    fix bad syntax on export

--- src/xhr/src/xhr.d.ts
+++ src/xhr/src/xhr.d.ts
@@ -1,4 +1,5 @@
 import xhr from './xhr'
 
-export default defaultXhr(options: XhrOptions): Promise<any>
+export default function defaultXhr(options: XhrOptions): Promise<any>
+
 export function xhrFactory({ baseURL }: { baseURL: string }): xhr
