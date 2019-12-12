93c8367a44dc1265e81d09fd1a3d4b68b79ef1c9
Rob Levin <rob@getethos.com>
2019-12-12 12:20:01 -0800

    Attempting to get a d.ts type def started

---
+++ src/components/index.d.ts
@@ -0,0 +1,41 @@
+// export { Logo } from './Logo'
+import { Button } from './Button'
+declare module 'Button'
+
+// export { CloudinaryImage, CLOUDINARY_CLOUD_NAME } from './Images'
+// export { COLORS } from './Colors'
+// export { Layout } from './Layout'
+// export { Media } from './Media'
+// export { Select } from './Select'
+// export { Spacer } from './Spacer'
+// export { ValueProps } from './ValueProps'
+// export { RadioButtonGroup } from './RadioButtons'
+// export { TextInput } from './TextInput'
+// export { PasswordInput } from './PasswordInput'
+// export { TextAreaInput } from './TextAreaInput'
+// export { EmailInput } from './EmailInput'
+// export { TextMaskedInput } from './TextMaskedInput'
+// export { InfoMessage } from './InfoMessage'
+// export { ZipInput } from './ZipInput'
+// export { CheckboxInput } from './CheckboxInput'
+// export { Form } from './Form'
+// export {
+//   BirthdateInput,
+//   BirthdateInputValidators,
+// } from './BirthdateInput/BirthdateInput'
+// export { UniversalNavbar } from './UniversalNavbar/UniversalNavbar'
+// export { ButtonSelectGroup } from './ButtonSelectGroup/ButtonSelectGroup'
+// export { NumberInput } from './NumberInput/NumberInput'
+// export { OPTION_BUTTON_STYLES } from './ButtonSelectGroup/OptionButton'
+
+// export {
+//   Body,
+//   Caption,
+//   Footnote,
+//   TitleSmall,
+//   TitleMedium,
+//   TitleLarge,
+//   TitleXLarge,
+//   TitleXXLarge,
+//   Link,
+// } from './Type'
