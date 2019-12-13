d7b562481ce882630d6f089dafc214bdecd9f555
Rob Levin <rob@getethos.com>
2019-12-12 17:53:31 -0800

    Bring back in original .js files that I accidentally overwrote

--- src/validators/BirthdateInputValidator.js
+++ src/validators/BirthdateInputValidator.js
@@ -1,64 +1,83 @@
-"use strict";
-exports.__esModule = true;
-var getDayjs_js_1 = require("../helpers/getDayjs.js");
-exports.DATE_FORMATS = ['mm/dd/yyyy', 'mm/yyyy', 'mm/yy'];
-exports.dateMaskByFormat = {
-    'mm/dd/yyyy': [/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/],
-    'mm/yyyy': [/\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/],
-    'mm/yy': [/\d/, /\d/, '/', /\d/, /\d/]
-};
-exports.dateRegexByFormat = {
-    'mm/dd/yyyy': /\d\d\/\d\d\/\d\d\d\d/,
-    'mm/yyyy': /\d\d\/\d\d\d\d/,
-    'mm/yy': /\d\d\/\d\d/
-};
-exports.dateStringMatchesFormat = function (cleansedDateString, dateFormat) {
-    var pattern = exports.dateRegexByFormat[dateFormat];
-    var matchesFormat = pattern.test(cleansedDateString);
-    if (!matchesFormat) {
-        return 'Please enter a valid date.';
-    }
-    return '';
-};
-exports.getMaxDateValidator = function (props) {
-    var maxDate = props.maxDate, customErrorMessage = props.customErrorMessage, dateFormat = props.dateFormat;
-    return function (value) {
-        if (value == null || value === '')
-            return '';
-        var date = getDayjs_js_1["default"](maxDate);
-        var dayjsFormat = dateFormat.toUpperCase();
-        var errorMessage = customErrorMessage ||
-            "Please enter a date before " + date.format(dayjsFormat);
-        return getDayjs_js_1["default"](value).isBefore(date) ? '' : errorMessage;
-    };
-};
-exports.getMinDateValidator = function (props) {
-    var minDate = props.minDate, customErrorMessage = props.customErrorMessage, dateFormat = props.dateFormat;
-    return function (value) {
-        // Should pass if there is no value
-        if (value == null || value === '')
-            return '';
-        var date = getDayjs_js_1["default"](minDate);
-        var dayjsFormat = dateFormat.toUpperCase();
-        var errorMessage = customErrorMessage ||
-            "Please enter a date before " + date.format(dayjsFormat);
-        return getDayjs_js_1["default"](value).isAfter(date) ? '' : errorMessage;
-    };
-};
-exports.validateMinMaxDateFactory = function (props) {
-    var minBirthdate = props.minBirthdate, maxBirthdate = props.maxBirthdate, minAge = props.minAge, maxAge = props.maxAge, dateFormat = props.dateFormat, customErrorMessage = props.customErrorMessage;
-    return function (value) {
-        var dateRangeErrorMessage = customErrorMessage || "Sorry, you must be " + minAge + "\u2013" + maxAge + ".";
-        var maxError = exports.getMaxDateValidator({
-            maxDate: maxBirthdate,
-            customErrorMessage: dateRangeErrorMessage,
-            dateFormat: dateFormat
-        })(value);
-        var minError = exports.getMinDateValidator({
-            minDate: minBirthdate,
-            customErrorMessage: dateRangeErrorMessage,
-            dateFormat: dateFormat
-        })(value);
-        return minError.length || maxError.length ? dateRangeErrorMessage : '';
-    };
-};
+import dayjs from '../helpers/getDayjs.js'
+
+export const DATE_FORMATS = ['mm/dd/yyyy', 'mm/yyyy', 'mm/yy']
+
+export const dateMaskByFormat = {
+  'mm/dd/yyyy': [/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/],
+  'mm/yyyy': [/\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/],
+  'mm/yy': [/\d/, /\d/, '/', /\d/, /\d/],
+}
+
+export const dateRegexByFormat = {
+  'mm/dd/yyyy': /\d\d\/\d\d\/\d\d\d\d/,
+  'mm/yyyy': /\d\d\/\d\d\d\d/,
+  'mm/yy': /\d\d\/\d\d/,
+}
+
+export const dateStringMatchesFormat = (cleansedDateString, dateFormat) => {
+  const pattern = dateRegexByFormat[dateFormat]
+  const matchesFormat = pattern.test(cleansedDateString)
+  if (!matchesFormat) {
+    return 'Please enter a valid date.'
+  }
+  return ''
+}
+
+export const getMaxDateValidator = (props) => {
+  const { maxDate, customErrorMessage, dateFormat } = props
+
+  return (value) => {
+    if (value == null || value === '') return ''
+    const date = dayjs(maxDate)
+    const dayjsFormat = dateFormat.toUpperCase()
+    const errorMessage =
+      customErrorMessage ||
+      `Please enter a date before ${date.format(dayjsFormat)}`
+    return dayjs(value).isBefore(date) ? '' : errorMessage
+  }
+}
+
+export const getMinDateValidator = (props) => {
+  const { minDate, customErrorMessage, dateFormat } = props
+
+  return (value) => {
+    // Should pass if there is no value
+    if (value == null || value === '') return ''
+    const date = dayjs(minDate)
+    const dayjsFormat = dateFormat.toUpperCase()
+    const errorMessage =
+      customErrorMessage ||
+      `Please enter a date before ${date.format(dayjsFormat)}`
+    return dayjs(value).isAfter(date) ? '' : errorMessage
+  }
+}
+
+export const validateMinMaxDateFactory = (props) => {
+  const {
+    minBirthdate,
+    maxBirthdate,
+    minAge,
+    maxAge,
+    dateFormat,
+    customErrorMessage,
+  } = props
+
+  return (value) => {
+    const dateRangeErrorMessage =
+      customErrorMessage || `Sorry, you must be ${minAge}–${maxAge}.`
+
+    const maxError = getMaxDateValidator({
+      maxDate: maxBirthdate,
+      customErrorMessage: dateRangeErrorMessage,
+      dateFormat,
+    })(value)
+
+    const minError = getMinDateValidator({
+      minDate: minBirthdate,
+      customErrorMessage: dateRangeErrorMessage,
+      dateFormat,
+    })(value)
+
+    return minError.length || maxError.length ? dateRangeErrorMessage : ''
+  }
+}
--- src/validators/EmailValidator.js
+++ src/validators/EmailValidator.js
@@ -1,9 +1,10 @@
-"use strict";
-exports.__esModule = true;
 // Regex pulled from here: https://stackoverflow.com/a/46181
-var validEmailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-var EMAIL_FORMAT_ERROR_MESSAGE = 'Please enter a valid email.';
-var EmailFormatValidator = function (email) {
-    return validEmailRegex.test(email) ? '' : EMAIL_FORMAT_ERROR_MESSAGE;
-};
-exports["default"] = EmailFormatValidator;
+const validEmailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+const EMAIL_FORMAT_ERROR_MESSAGE = 'Please enter a valid email.'
+
+const EmailFormatValidator = (email) => {
+  return validEmailRegex.test(email) ? '' : EMAIL_FORMAT_ERROR_MESSAGE
+}
+
+export default EmailFormatValidator
