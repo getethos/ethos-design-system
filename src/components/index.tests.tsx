b5a3342d26b4060fe5a298cb527ea9bb6975fd28
Rob Levin <rob@getethos.com>
2019-12-30 16:15:33 -0800

    Stronger types for CheckboxInput. Also, Provides usage in index.tests.tsx.

--- src/components/index.tests.tsx
+++ src/components/index.tests.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react'
+import { CheckboxInput } from './index'
 import { Faq } from './index'
+
+// Usage: `yarn test:types` -- see [package.json](../../package.json):
 
 const demoQuestions = [
   {
@@ -13,8 +16,26 @@ const demoQuestions = [
 ]
 
 // Localized test
-class MyTest extends React.Component<any, any> {
+class FaqTest extends React.Component<any, any> {
   render() {
     return <Faq questions={demoQuestions} />
+  }
+}
+
+class CheckboxInputTest extends React.Component<any, any> {
+  render() {
+    return (
+      <>
+        <CheckboxInput
+          name="name: string -- is required"
+          data-tid="data-tid: string -- is required"
+        >
+          I agree to the{' '}
+          <a href="/" target="_blank">
+            Agreement
+          </a>
+        </CheckboxInput>
+      </>
+    )
   }
 }
