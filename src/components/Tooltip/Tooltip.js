55b5b06036cf57dc9f97a24ff13dc8646698b118
Tamir Karssli <tkarssli@gmail.com>
2020-01-03 15:12:58 -0800

    Mobile Popup

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -1,32 +1,65 @@
 import React, { useState, useRef } from 'react'
 import PropTypes from 'prop-types'
-import uuidv4 from 'uuid/v4'
-import {} from '../index'
+import { DialogOverlay, DialogContent } from '@reach/dialog'
+import { useTransition, animated } from 'react-spring'
 import { Manager, Reference, Popper } from 'react-popper'
+
+import { TitleLarge, Body, Footnote } from '../index'
 
 import styles from './Tooltip.module.scss'
 
-export const Tooltip = ({ placement, behavior }) => {
-  const [visible, setVisible] = useState(false)
+let AnimatedDialogOverlay = animated(DialogOverlay)
+let AnimatedDialogContent = animated(DialogContent)
+
+export const Tooltip = ({ label, placement, behavior, children }) => {
+  const [visibleFromHover, setVisibilityFromHover] = useState(false)
+  const [visibleFromClick, setVisibilityFromClick] = useState(false)
+
+  const transitions = useTransition(visibleFromClick, null, {
+    from: { opacity: 0, transform: 'translate3d(0px, -50px, 0px)' },
+    enter: { opacity: 1, transform: 'translate3d(0px, 0px, 0px)' },
+    leave: { opacity: 0, transform: 'translate3d(0px, -50px, 0px)' },
+  })
+
+  const renderMobileModal = transitions.map(
+    ({ item, key, props }) =>
+      item && (
+        <AnimatedDialogOverlay
+          key={key}
+          style={{ opacity: props.opacity }}
+          onDismiss={() => setVisibilityFromClick(false)}
+        >
+          <AnimatedDialogContent
+            className={styles.mobileModal}
+            aria-label={`${label} Tooltip`}
+            style={props}
+          >
+            <button
+              className={styles.closeButton}
+              onClick={() => setVisibilityFromClick(false)}
+            >
+              {Tooltip.SVGS.closeButton}
+            </button>
+            <div className={styles.label}>
+              <TitleLarge.Sans.Regular400>{label}</TitleLarge.Sans.Regular400>
+            </div>
+            <Body.Regular400>{children}</Body.Regular400>
+          </AnimatedDialogContent>
+        </AnimatedDialogOverlay>
+      )
+  )
 
   const modifiers = {
     preventOverflow: {
       enabled: true,
     },
-    [behavior]: Tooltip.BEHAVIOR_TYPES[behavior],
-  }
-
-  const onHover = (visibility) => {
-    return () => setVisible(visibility)
-  }
-
-  const tooltipStyle = {
-    opacity: visible ? '1' : '0',
+    flip: { enabled: true, boundariesElement: 'viewport', behavior: 'flip' },
   }
 
   const referenceProps = {
-    onMouseOver: onHover(true),
-    onMouseOut: onHover(false),
+    onMouseOver: () => setVisibilityFromHover(true),
+    onMouseOut: () => setVisibilityFromHover(false),
+    onClick: () => setVisibilityFromClick(true),
   }
 
   const popperProps = {
@@ -35,37 +68,32 @@ export const Tooltip = ({ placement, behavior }) => {
     modifiers,
   }
 
-  return (
+  const contentBoxClasses = [
+    styles.contentBox,
+    visibleFromHover ? styles.visible : styles.hidden,
+  ]
+
+  const renderTooltip = (
     <div className={styles.root}>
       <Manager>
         <Reference>
           {({ ref }) => (
             <div className={styles.icon} ref={ref} {...referenceProps}>
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M8.00064 1.33203C4.32065 1.33203 1.33398 4.31869 1.33398 7.99868C1.33398 11.6787 4.32065 14.6653 8.00064 14.6653C11.6806 14.6653 14.6673 11.6787 14.6673 7.99868C14.6673 4.31869 11.6806 1.33203 8.00064 1.33203ZM8.66731 11.332H7.33398V7.33202H8.66731V11.332ZM8.66731 5.99869H7.33398V4.66536H8.66731V5.99869Z"
-                  fill="black"
-                  fill-opacity={visible ? '1' : '0.2'}
-                />
-              </svg>
+              {visibleFromHover
+                ? Tooltip.SVGS.iconHover
+                : Tooltip.SVGS.iconStatic}
             </div>
           )}
         </Reference>
-        <div className={styles.container}>
+        <div className={styles.popperContainer}>
           <Popper {...popperProps}>
             {({ ref, style, placement, arrowProps }) => (
               <div
-                className={styles.box}
+                className={contentBoxClasses.join(' ')}
                 ref={ref}
-                style={Object.assign({}, tooltipStyle, style)}
+                style={style}
               >
-                Popper element
+                <Footnote.Regular400>{children}</Footnote.Regular400>
                 <div
                   ref={arrowProps.ref}
                   className={styles.arrow}
@@ -79,6 +107,60 @@ export const Tooltip = ({ placement, behavior }) => {
       </Manager>
     </div>
   )
+
+  return (
+    <>
+      {renderMobileModal}
+      {renderTooltip}
+    </>
+  )
+}
+
+Tooltip.SVGS = {
+  closeButton: (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z"
+        fill="black"
+      />
+    </svg>
+  ),
+  iconHover: (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8.00064 1.33203C4.32065 1.33203 1.33398 4.31869 1.33398 7.99868C1.33398 11.6787 4.32065 14.6653 8.00064 14.6653C11.6806 14.6653 14.6673 11.6787 14.6673 7.99868C14.6673 4.31869 11.6806 1.33203 8.00064 1.33203ZM8.66731 11.332H7.33398V7.33202H8.66731V11.332ZM8.66731 5.99869H7.33398V4.66536H8.66731V5.99869Z"
+        fill="black"
+        fillOpacity={'1'}
+      />
+    </svg>
+  ),
+  iconStatic: (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8.00064 1.33203C4.32065 1.33203 1.33398 4.31869 1.33398 7.99868C1.33398 11.6787 4.32065 14.6653 8.00064 14.6653C11.6806 14.6653 14.6673 11.6787 14.6673 7.99868C14.6673 4.31869 11.6806 1.33203 8.00064 1.33203ZM8.66731 11.332H7.33398V7.33202H8.66731V11.332ZM8.66731 5.99869H7.33398V4.66536H8.66731V5.99869Z"
+        fill="black"
+        fillOpacity={'0.2'}
+      />
+    </svg>
+  ),
 }
 
 Tooltip.PLACEMENT_TYPES = {
@@ -88,19 +170,13 @@ Tooltip.PLACEMENT_TYPES = {
   BOTTOM: 'bottom',
   AUTO: 'auto',
 }
-
-Tooltip.BEHAVIOR_TYPES = {
-  flip: { enabled: true, boundariesElement: 'viewport', behavior: 'flip' },
-}
-
 Tooltip.defaultProps = {
   placement: Tooltip.PLACEMENT_TYPES.TOP,
-  behavior: 'flip',
 }
 
 Tooltip.propTypes = {
   placement: PropTypes.oneOf(Object.values(Tooltip.PLACEMENT_TYPES)),
-  behavior: PropTypes.oneOf(Object.keys(Tooltip.BEHAVIOR_TYPES)),
+  label: PropTypes.string.isRequired,
 }
 
 export default Tooltip
--- src/components/Tooltip/Tooltip.md
+++ src/components/Tooltip/Tooltip.md
@@ -1,57 +1,50 @@
 ```jsx
 import React, { useRef, useState, useEffect } from 'react'
-
-const Container = ({ children }) => {
-  return (
+;<>
+  <div
+    style={{
+      overflow: 'scroll',
+      height: '200px',
+      width: '100%',
+    }}
+  >
     <div
       style={{
-        overflow: 'scroll',
-        height: '200px',
-        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
       }}
     >
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-        }}
-      >
-        Top aligned with insufficient space and flip behavior ->
-        <span>
-          <Tooltip behavior={'flip'} placement={'top'} />
-        </span>
-      </div>
-      <div
-        style={{
-          height: '100px',
-          display: 'flex',
-          alignItems: 'flex-end',
-        }}
-      >
-        Top aligned with enough space ->
-        <span>
-          <Tooltip behavior={'flip'} placement={'top'} />
-        </span>
-      </div>
-      <br />
-      Scroll Down!
-      <div
-        style={{
-          height: '600px',
-          display: 'flex',
-          alignItems: 'center',
-        }}
-      >
-        Hover over this thing for more information ->
-        <span>
-          <Tooltip behavior={'flip'} placement={'top'} />
-        </span>
-      </div>
+      Top aligned with insufficient space and flip behavior ->
+      <span>
+        <Tooltip label={'Flip'}>Hi!</Tooltip>
+      </span>
     </div>
-  )
-}
-
-;<>
-  <Container></Container>
+    <div
+      style={{
+        height: '100px',
+        display: 'flex',
+        alignItems: 'flex-end',
+      }}
+    >
+      Top aligned with enough space ->
+      <span>
+        <Tooltip label={'Top Aligned'}> Hi again!</Tooltip>
+      </span>
+    </div>
+    <br />
+    Scroll Down!
+    <div
+      style={{
+        height: '600px',
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      Hover over this thing for more information ->
+      <span>
+        <Tooltip label={'Information'}> More Information!</Tooltip>
+      </span>
+    </div>
+  </div>
 </>
 ```
