21ecbf2f0827383dae30c5653097fffdfe4e7a7c
Tamir Karssli <tkarssli@gmail.com>
2020-01-08 14:26:46 -0800

    mock for window

--- src/components/Tooltip/Tooltip.test.js
+++ src/components/Tooltip/Tooltip.test.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Tooltip } from './Tooltip.js'
 import renderer from 'react-test-renderer'
+import windowMock from '../../mocks/windowMock'
+
+Object.assign(window, windowMock)
 
 describe('Tooltip', () => {
   describe('API', () => {
