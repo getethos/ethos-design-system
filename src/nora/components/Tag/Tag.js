45e66ca7c1ff222bc98f5aab5095041aaec86743
Stephanie Liu <12839832+stephaniel96@users.noreply.github.com>
2020-01-13 18:53:56 -0800

    [Nora] Tag Component (#222)

---
+++ src/nora/components/Tag/Tag.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './Tag.module.scss'
+
+const Tag = ({ children, type }) => {
+  let colorClass
+  switch (type) {
+    case 'approved':
+      colorClass = styles.Approved
+      break
+    case 'rejected':
+      colorClass = styles.Rejected
+      break
+    default:
+      colorClass = styles.Default
+  }
+
+  return <div className={[styles.Tag, colorClass].join(' ')}>{children}</div>
+}
+
+Tag.propTypes = {
+  type: PropTypes.oneOf(['approved', 'rejected']),
+  children: PropTypes.string.isRequired,
+}
+
+Tag.defaultProps = {
+  type: undefined,
+}
+
+export default Tag
