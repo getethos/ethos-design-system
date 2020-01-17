c7afc55fbf399fa36568f6c1b038aaf6e889c753
Rob Levin <roblevintennis@gmail.com>
2020-01-17 14:03:28 -0800

    Conditionally paging buttons and add labelCopy prop to grid (#225)
    * Adds conditionallyRenderPagingButtons so we only render the pagination buttons if there is more then a single page to display (it looks odd to have buttons for paging when there is nothing to page).
    
    * Missed commiting this
    
    * v1.1.55

--- src/components/Grid/Pagination.js
+++ src/components/Grid/Pagination.js
@@ -1,7 +1,7 @@
-import React, { memo } from 'react'
-import { usePagination } from './usePagination'
 import PropTypes from 'prop-types'
+import React, { memo } from 'react'
 import styles from './Pagination.module.scss'
+import { usePagination } from './usePagination'
 
 export const Pagination = memo(({ fetchPageCallback, renderCallback }) => {
   if (!fetchPageCallback || !renderCallback) {
@@ -14,32 +14,42 @@ export const Pagination = memo(({ fetchPageCallback, renderCallback }) => {
     fetchPage: fetchPageCallback,
   })
 
+  const conditionallyRenderPagingButtons = () => {
+    if (pagingState.pageCount > 1) {
+      return (
+        <nav aria-label="pagination" className={styles.pagination}>
+          <button
+            className={[
+              styles.paginationButtons,
+              styles.paginationButtonsLeft,
+            ].join(' ')}
+            onClick={() => fetchPage(1)}
+            aria-label="Goto Page 1"
+          >
+            &laquo;
+          </button>
+          {getPaginationNumbers()}
+          <button
+            className={[
+              styles.paginationButtons,
+              styles.paginationButtonsRight,
+            ].join(' ')}
+            onClick={() => fetchPage(pagingState.pageCount)}
+            aria-label={`Goto Page ${pagingState.pageCount}`}
+          >
+            &raquo;
+          </button>
+        </nav>
+      )
+    } else {
+      return null
+    }
+  }
+
   return (
     <>
       {renderCallback(pagingState.items)}
-      <nav aria-label="pagination" className={styles.pagination}>
-        <button
-          className={[
-            styles.paginationButtons,
-            styles.paginationButtonsLeft,
-          ].join(' ')}
-          onClick={() => fetchPage(1)}
-          aria-label="Goto Page 1"
-        >
-          &laquo;
-        </button>
-        {getPaginationNumbers()}
-        <button
-          className={[
-            styles.paginationButtons,
-            styles.paginationButtonsRight,
-          ].join(' ')}
-          onClick={() => fetchPage(pagingState.pageCount)}
-          aria-label={`Goto Page ${pagingState.pageCount}`}
-        >
-          &raquo;
-        </button>
-      </nav>
+      {conditionallyRenderPagingButtons()}
     </>
   )
 })
