e4011563af054aa50b7307f9d61cbd63056d7c1c
Rob Levin <rob@getethos.com>
2020-01-17 07:57:03 -0800

    Adds conditionallyRenderPagingButtons so we only render the pagination buttons if there is more then a single page to display (it looks odd to have buttons for paging when there is nothing to page).

--- src/components/Grid/Pagination.test.js
+++ src/components/Grid/Pagination.test.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import TestRenderer, { act } from 'react-test-renderer'
-
 import { Pagination } from './Pagination'
 import styles from './Pagination.module.scss'
-
-const responseStub = {
-  page: 1,
-  itemCount: 2,
-  total: 6,
-  pageCount: 3,
-  items: [{ id: 1 }, { id: 2 }],
-}
 
 describe('Pagination Component', () => {
   let tree
   let fetchFn
   let renderFn
+
+  const responseStub = {
+    page: 1,
+    itemCount: 2,
+    total: 6,
+    pageCount: 3,
+    items: [{ id: 1 }, { id: 2 }],
+  }
 
   beforeEach(() => {
     fetchFn = jest.fn()
@@ -41,13 +40,14 @@ describe('Pagination Component', () => {
   })
 
   it('fetches', async () => {
-    tree = TestRenderer.create(
-      <Pagination fetchPageCallback={fetchFn} renderCallback={renderFn} />
-    )
+    await act(async () => {
+      tree = TestRenderer.create(
+        <Pagination fetchPageCallback={fetchFn} renderCallback={renderFn} />
+      )
+    })
     const root = tree.root
     const buttons = root.findAllByType('button')
     const [first] = buttons
-
     await act(async () => {
       first.props.onClick()
     })
@@ -81,5 +81,41 @@ describe('Pagination Component', () => {
     const buttons = root.findAllByType('button')
     const firstPageNumberButton = buttons[1]
     expect(firstPageNumberButton.props.className).toContain(styles.active)
+  })
+})
+
+describe('Hide Pagination', () => {
+  let tree
+  let fetchFn
+  let renderFn
+  const singlePageResponseStub = {
+    page: 1,
+    itemCount: 2,
+    total: 2,
+    pageCount: 1,
+    items: [{ id: 1 }, { id: 2 }],
+  }
+
+  beforeEach(() => {
+    fetchFn = jest.fn()
+    renderFn = jest.fn()
+    fetchFn.mockReturnValue(singlePageResponseStub)
+  })
+
+  afterEach(() => {
+    tree = null
+    fetchFn = null
+    renderFn = null
+  })
+
+  it('hides the pagination if only a single page worth of items', async () => {
+    await act(async () => {
+      tree = TestRenderer.create(
+        <Pagination fetchPageCallback={fetchFn} renderCallback={renderFn} />
+      )
+    })
+    const root = tree.root
+    const buttons = root.findAllByType('button')
+    expect(buttons.length).toEqual(0)
   })
 })
