0534db6e6b854f71efc9c7f7dc62987c2ae9652e
Rob Levin <rob@getethos.com>
2020-01-20 10:23:04 -0800

    Some pagination dos refactoring

--- src/components/Grid/Pagination2.js
+++ src/components/Grid/Pagination2.js
@@ -1,75 +1,34 @@
-import React from 'react'
+import PropTypes from 'prop-types'
+import React, { memo } from 'react'
 import styles from './Pagination.module.scss'
 
-console.log('Pagination2 loading...')
-export const Pagination2 = ({ currentPage, pageCount, onClickCallback }) => {
-  // Function to help calculate the pagination numbers we should display
-  const getPaginationNumbers = () => {
-    const pageNumbers = []
-    const totalPages = pageCount
-    let paginationNumbers
+import { usePagination } from './usePagination2'
 
-    if (totalPages) {
-      for (let i = 1; i <= totalPages; i++) {
-        pageNumbers.push(i)
-      }
-      paginationNumbers = pageNumbers.map((number) => {
-        const isCurrentPage = currentPage === number
-        const klasses = isCurrentPage
-          ? `${styles.paginationButtons} ${styles.active}`
-          : styles.paginationButtons
-
-        return (
-          <button
-            key={number}
-            // eslint-disable-next-line
-            {...isCurrentPage && { 'aria-current': 'page' }}
-            className={klasses}
-            onClick={() => onClickCallback(number)}
-            aria-label={`Goto Page ${number}`}
-          >
-            {number}
-          </button>
-        )
-      })
+export const Pagination2 = memo(
+  ({ currentPage, pageCount, fetchPageCallback }) => {
+    if (
+      typeof currentPage !== 'number' ||
+      typeof pageCount !== 'number' ||
+      !fetchPageCallback
+    ) {
+      throw Error(
+        'Pagination requires currentPage, pageCount, and fetchPageCallback parameters'
+      )
     }
-    return paginationNumbers
-  }
+    const {
+      getPaginationNumbers,
+      conditionallyRenderPagingButtons,
+    } = usePagination({
+      fetchPageCallback,
+    })
 
-  const previousExists = currentPage > 1
-  const nextExists = currentPage < pageCount
-
-  if (pageCount > 1) {
-    return (
-      <nav aria-label="pagination" className={styles.pagination}>
-        {previousExists && (
-          <button
-            className={[
-              styles.paginationButtons,
-              styles.paginationButtonsLeft,
-            ].join(' ')}
-            onClick={() => onClickCallback(currentPage - 1)}
-            aria-label={`Goto Page ${currentPage - 1}`}
-          >
-            &laquo;
-          </button>
-        )}
-        {getPaginationNumbers()}
-        {nextExists && (
-          <button
-            className={[
-              styles.paginationButtons,
-              styles.paginationButtonsRight,
-            ].join(' ')}
-            onClick={() => onClickCallback(currentPage + 1)}
-            aria-label={`Goto Page ${currentPage + 1}`}
-          >
-            &raquo;
-          </button>
-        )}
-      </nav>
-    )
-  } else {
-    return null
+    return conditionallyRenderPagingButtons(pageCount, currentPage)
   }
+)
+
+Pagination2.propTypes = {
+  fetchPageCallback: PropTypes.func.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  pageCount: PropTypes.number.isRequired,
 }
+Pagination2.displayName = 'Pagination2'
