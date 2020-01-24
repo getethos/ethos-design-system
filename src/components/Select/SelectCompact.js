5fad91cf515f895cce45b3f419efd87231e5bf00
Rob Levin <rob@getethos.com>
2020-01-23 16:24:42 -0800

    Pass through the className props

--- src/components/Select/SelectCompact.js
+++ src/components/Select/SelectCompact.js
@@ -2,6 +2,11 @@ import React from 'react'
 import { Select } from './Select'
 import styles from './SelectCompact.scss'
 
-export const SelectCompact = (props) => {
-  return <Select className="SelectCompact" {...props} />
+export const SelectCompact = ({ className, ...rest }) => {
+  const props = {
+    className: `${className ? className : ''} SelectCompact`,
+    ...rest,
+  }
+
+  return <Select {...props} />
 }
