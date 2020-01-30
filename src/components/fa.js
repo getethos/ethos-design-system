326482fc64267e915b005b119823cae4c3ab75c6
Rob Levin <rob@getethos.com>
2020-01-29 20:46:50 -0800

    Switch to 'deep imports' per fontawesome recommendation and Stephs finding

--- src/components/fa.js
+++ src/components/fa.js
@@ -11,5 +11,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 
 // We need to add any icons we want to use here on an as needed basis. This will
 // hopefully help to keep the footprint small, and is the recommended approach.
-import { faSearch, faCircleNotch } from '@fortawesome/pro-regular-svg-icons'
+// Since we aren't yet achieving treeshaking, "deep imports" is the recommendation:
+// See https://fontawesome.com/how-to-use/with-the-api/other/tree-shaking
+import { faSearch } from '@fortawesome/pro-regular-svg-icons/faSearch'
+import { faCircleNotch } from '@fortawesome/pro-regular-svg-icons/faCircleNotch'
 library.add(faSearch, faCircleNotch)
