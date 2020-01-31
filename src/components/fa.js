b55bb81a34cb3204d70c717284f507639c742595
Rob Levin <rob@getethos.com>
2020-01-31 15:49:55 -0800

    Adds the font awesome icons to the library

--- src/components/fa.js
+++ src/components/fa.js
@@ -13,11 +13,31 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 // hopefully help to keep the footprint small, and is the recommended approach.
 // Since we aren't yet achieving treeshaking, "deep imports" is the recommendation:
 // See https://fontawesome.com/how-to-use/with-the-api/other/tree-shaking
-import { faSearch } from '@fortawesome/pro-regular-svg-icons/faSearch'
 
-// REMINDER! Each import must be a separate named import and use "deep import"
+//////////////////////////////////////////////////////////////////////
+// REMINDER! each import must be a separate "deep import" as below  //
+//////////////////////////////////////////////////////////////////////
+
+import { faAlignLeft } from '@fortawesome/pro-solid-svg-icons/faAlignLeft'
 import { faCircleNotch } from '@fortawesome/pro-regular-svg-icons/faCircleNotch'
-import { faExclamationTriangle } from '@fortawesome/pro-solid-svg-icons/faExclamationTriangle'
-import { faWindowClose } from '@fortawesome/pro-solid-svg-icons/faWindowClose'
+import { faExclamationTriangle } from '@fortawesome/pro-light-svg-icons/faExclamationTriangle'
+import { faSearch } from '@fortawesome/pro-regular-svg-icons/faSearch'
+import { faAsterisk } from '@fortawesome/pro-light-svg-icons/faAsterisk'
+import { faExchange } from '@fortawesome/pro-regular-svg-icons/faExchange'
+import { faClock } from '@fortawesome/pro-light-svg-icons/faClock'
+import { faUserCheck } from '@fortawesome/pro-light-svg-icons/faUserCheck'
+import { faCheckSquare } from '@fortawesome/pro-regular-svg-icons/faCheckSquare'
+import { faWindowClose } from '@fortawesome/pro-light-svg-icons/faWindowClose'
 
-library.add(faSearch, faCircleNotch, faExclamationTriangle, faWindowClose)
+library.add(
+  faAlignLeft,
+  faAsterisk,
+  faCheckSquare,
+  faCircleNotch,
+  faClock,
+  faExchange,
+  faExclamationTriangle,
+  faSearch,
+  faUserCheck,
+  faWindowClose
+)
