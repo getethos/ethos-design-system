a99d5a09c77c585e460912539e90739376da4659
Cole <c7hudson@gmail.com>
2020-01-31 16:25:12 -0800

    fixes/linting

--- src/components/UniversalNavbar/UniversalNavbar.js
+++ src/components/UniversalNavbar/UniversalNavbar.js
@@ -96,7 +96,9 @@ class UniversalNavbar extends React.Component {
         <NavLink
           href={link.href}
           LinkComponent={link.href !== '/login/' ? LinkComponent : null}
-          className={location.pathname === link.href ? styles.currentPage : null}
+          className={
+            location.pathname === link.href ? styles.currentPage : null
+          }
         >
           {link.title}
         </NavLink>
