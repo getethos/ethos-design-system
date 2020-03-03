2bf52538bd00b3d6be967da0b824605fec1266d9
stephanieL96 <12839832+stephaniel96@users.noreply.github.com>
2020-03-03 11:53:14 -0800

    Linting and testing

--- src/nora/components/Icon/Icon.js
+++ src/nora/components/Icon/Icon.js
@@ -3,9 +3,7 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const Icon = ({ iconPrefix, iconName, className }) => {
-  return (
-    <FontAwesomeIcon className={className} icon={[iconPrefix, iconName]} />
-  )
+  return <FontAwesomeIcon className={className} icon={[iconPrefix, iconName]} />
 }
 
 Icon.propTypes = {
