d050464f4b2f03bef7573cfb3942e4e16d701579
Cole <c7hudson@gmail.com>
2020-03-02 16:30:58 -0800

    update links, tweak laptop lower end of viewport

--- src/components/UniversalNavbarExpanded/constants.js
+++ src/components/UniversalNavbarExpanded/constants.js
@@ -65,18 +65,127 @@ export const LINKS = {
     },
     {
       id: uuidv4(),
-      href: '/life/life-insurance-basics/',
+      href: null,
       title: 'Life insurance basics',
+      subnav: {
+        cta: {
+          href: '/life/life-insurance-basics/',
+          title: 'Life insurance 101',
+          subcopy:
+            'Wondering where to start? We’ve broken down the essentials for you.',
+          id: uuidv4(),
+        },
+        items: [
+          {
+            id: uuidv4(),
+            href: '/life/term-group-whole/',
+            title:
+              'What are the main types of life insurance and how do they work?',
+          },
+          {
+            id: uuidv4(),
+            href: '/life/underwriting-explained/',
+            title: 'What is underwriting and how long does it take?',
+          },
+          {
+            id: uuidv4(),
+            href: '/life/pick-beneficiary/',
+            title: 'Choosing your beneficiary',
+          },
+          {
+            id: uuidv4(),
+            href: '/life/who-needs-sp/',
+            title: "What if I don't work?",
+          },
+          {
+            id: uuidv4(),
+            href: '/life/employer-sponsored-life-insurance/',
+            title: 'Is my life insurance through work enough?',
+          },
+        ],
+      },
     },
     {
       id: uuidv4(),
-      href: '/why-ethos/',
+      href: null,
       title: 'Why Ethos',
+      subnav: {
+        cta: {
+          href: '/why-ethos/',
+          title: 'The Ethos Difference',
+          subcopy:
+            'We put people before profit. Find out how we bring our policyholders the best experience possible.',
+          id: uuidv4(),
+        },
+        items: [
+          {
+            id: uuidv4(),
+            href: '/about/',
+            title: 'Our mission',
+          },
+          {
+            id: uuidv4(),
+            href: '/reviews/',
+            title: 'Reviews',
+          },
+          {
+            id: uuidv4(),
+            href: '/life/customer-stories/',
+            title: 'Customer stories',
+          },
+          {
+            id: uuidv4(),
+            href: '/life/ethosforgood/',
+            title: 'Ethos for Good',
+          },
+          {
+            id: uuidv4(),
+            href: '/friends/',
+            title: 'Refer a friend',
+          },
+        ],
+      },
     },
     {
       id: uuidv4(),
-      href: '/faq/',
+      href: null,
       title: 'Help Center',
+      subnav: {
+        cta: {
+          href: '/faq/',
+          title: 'FAQ',
+          subcopy:
+            'Life insurance can be complicated, but don’t worry. We’re here to help answer your questions.',
+          id: uuidv4(),
+        },
+        items: [
+          {
+            id: uuidv4(),
+            href: '/faq/life-insurance-basics/',
+            title: 'Questions about life insurance',
+          },
+          {
+            id: uuidv4(),
+            href: '/faq/ethos/',
+            title: 'Questions about Ethos',
+          },
+          {
+            id: uuidv4(),
+            href: '/faq/application/',
+            title: 'Questions about your application',
+          },
+          {
+            id: uuidv4(),
+            href: '/faq/policy/',
+            title: 'Questions about your policy',
+          },
+          {
+            id: uuidv4(),
+            href: '/contact-us/',
+            title: 'Contact us',
+          },
+        ],
+      },
     },
   ],
 }
