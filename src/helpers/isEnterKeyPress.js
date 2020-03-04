e6c95bdb4499b2534a59e6695a5c213124e8393a
Cole <c7hudson@gmail.com>
2020-03-04 13:35:25 -0800

    isEnterKeyPress simplification

--- src/helpers/isEnterKeyPress.js
+++ src/helpers/isEnterKeyPress.js
@@ -1,13 +1,9 @@
 import { codes } from './constants.js'
 
 export const isEnterKeyPress = (event) => {
-  if (
+  return (
     event.key === 'Enter' ||
     event.which === codes.RETURN ||
     event.keyCode === codes.RETURN
-  ) {
-    return true
-  } else {
-    return false
-  }
+  )
 }
