306aa2f89dc3ff7d7e5c0511a8019d036ab83d13
Cole <c7hudson@gmail.com>
2020-03-04 00:02:30 -0800

    fix tests

--- src/components/UniversalNavbarExpanded/constants.js
+++ src/components/UniversalNavbarExpanded/constants.js
@@ -1,6 +1,5 @@
-import uuidv4 from 'uuid/v4'
-
-// for demonstration in styleguide only!
+// for demonstration in styleguide & test files (.md/.test.js files) only!
+// real implementation of this structure should use uuid() for id values
 export const CMS_LINKS = {
   // These are used e.g. in the logo, icons and bottom of mobile nav:
   INDEX: { href: '/' },
@@ -9,7 +8,7 @@ export const CMS_LINKS = {
     title: 'Check my price',
   },
   // TALK: {
-  //     id: uuidv4(),
+  //     id: 'NAVLINKS_MOCK_ID_',
   //     href: '/',
   //     title: 'Talk to us',
   //   },
@@ -25,7 +24,7 @@ export const CMS_LINKS = {
   // These are the main top level link and their subnav items
   NAVLINKS: [
     {
-      id: uuidv4(),
+      id: 'NAVLINKS_MOCK_ID_1',
       href: null,
       title: 'What we offer',
       subnav: {
@@ -34,31 +33,31 @@ export const CMS_LINKS = {
           title: 'Our plans',
           subcopy:
             'Learn more about term life insurance and the plans and options you have available.',
-          id: uuidv4(),
+          id: 'NAVLINKS_MOCK_ID_2',
         },
         items: [
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_3',
             href: '/life/ethos-term-insurance/',
             title: 'What is term life insurance and how much does it cost?',
           },
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_4',
             href: '/how-it-works/',
             title: 'How our application process works',
           },
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_5',
             href: '/life/who-needs-life-insurance/',
             title: 'Do I need life insurance?',
           },
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_6',
             href: '/life/how-choose-right-type-life-insurance/',
             title: 'Choosing your coverage amount and term length',
           },
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_7',
             href: '/app/needs/',
             title: 'Coverage calculator',
           },
@@ -66,7 +65,7 @@ export const CMS_LINKS = {
       },
     },
     {
-      id: uuidv4(),
+      id: 'NAVLINKS_MOCK_ID_8',
       href: null,
       title: 'Life insurance basics',
       subnav: {
@@ -75,32 +74,32 @@ export const CMS_LINKS = {
           title: 'Life insurance 101',
           subcopy:
             'Wondering where to start? We’ve broken down the essentials for you.',
-          id: uuidv4(),
+          id: 'NAVLINKS_MOCK_ID_9',
         },
         items: [
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_10',
             href: '/life/term-group-whole/',
             title:
               'What are the main types of life insurance and how do they work?',
           },
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_11',
             href: '/life/underwriting-explained/',
             title: 'What is underwriting and how long does it take?',
           },
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_12',
             href: '/life/pick-beneficiary/',
             title: 'Choosing your beneficiary',
           },
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_13',
             href: '/life/who-needs-sp/',
             title: "What if I don't work?",
           },
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_14',
             href: '/life/employer-sponsored-life-insurance/',
             title: 'Is my life insurance through work enough?',
           },
@@ -108,7 +107,7 @@ export const CMS_LINKS = {
       },
     },
     {
-      id: uuidv4(),
+      id: 'NAVLINKS_MOCK_ID_15',
       href: null,
       title: 'Why Ethos',
       subnav: {
@@ -117,31 +116,31 @@ export const CMS_LINKS = {
           title: 'The Ethos Difference',
           subcopy:
             'We put people before profit. Find out how we bring our policyholders the best experience possible.',
-          id: uuidv4(),
+          id: 'NAVLINKS_MOCK_ID_16',
         },
         items: [
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_17',
             href: '/about/',
             title: 'Our mission',
           },
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_18',
             href: '/reviews/',
             title: 'Reviews',
           },
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_19',
             href: '/life/customer-stories/',
             title: 'Customer stories',
           },
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_20',
             href: '/life/ethosforgood/',
             title: 'Ethos for Good',
           },
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_21',
             href: '/friends/',
             title: 'Refer a friend',
           },
@@ -149,7 +148,7 @@ export const CMS_LINKS = {
       },
     },
     {
-      id: uuidv4(),
+      id: 'NAVLINKS_MOCK_ID_22',
       href: null,
       title: 'Help Center',
       subnav: {
@@ -158,31 +157,31 @@ export const CMS_LINKS = {
           title: 'FAQ',
           subcopy:
             'Life insurance can be complicated, but don’t worry. We’re here to help answer your questions.',
-          id: uuidv4(),
+          id: 'NAVLINKS_MOCK_ID_23',
         },
         items: [
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_24',
             href: '/faq/life-insurance-basics/',
             title: 'Questions about life insurance',
           },
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_25',
             href: '/faq/ethos/',
             title: 'Questions about Ethos',
           },
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_26',
             href: '/faq/application/',
             title: 'Questions about your application',
           },
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_27',
             href: '/faq/policy/',
             title: 'Questions about your policy',
           },
           {
-            id: uuidv4(),
+            id: 'NAVLINKS_MOCK_ID_28',
             href: '/contact-us/',
             title: 'Contact us',
           },
