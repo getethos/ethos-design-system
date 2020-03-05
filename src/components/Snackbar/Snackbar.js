d5052df8da6cf80851359e8e462007ca46eb0aa5
Rob Levin <rob@getethos.com>
2020-03-05 15:25:54 -0800

    Getting much closer -- handles ESC, a11y, documented

--- src/components/Snackbar/Snackbar.js
+++ src/components/Snackbar/Snackbar.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import PropTypes from 'prop-types'
 import { Portal } from '../Portal'
+import useOutsideClick from '../../hooks/a11y/useOutsideClick'
+import useOutsideEscape from '../../hooks/a11y/useOutsideEscape'
+import useHideAriaSiblings from '../../hooks/a11y/useHideAriaSiblings'
+import useTrapFocus from '../../hooks/a11y/useTrapFocus'
 
 /**
  * This is a base snackbar component meant to be unstyled and flexible so
@@ -8,11 +12,58 @@ import { Portal } from '../Portal'
  * Nora application uses this component. If you plan to use in another Ethos
  * application, **please consult design first**!
  */
-export const Snackbar = ({ id, children }) => {
-  return <Portal id={id}>{children}</Portal>
+export const Snackbar = ({
+  id,
+  onDismiss,
+  isOpen = false,
+  ariaLabelledBy,
+  ariaDescribedBy,
+  children,
+  ...rest
+}) => {
+  const snackbarRef = useRef(null)
+  useOutsideClick(snackbarRef, () => onDismiss(false))
+  useOutsideEscape(snackbarRef, () => onDismiss(false))
+  useTrapFocus(snackbarRef, isOpen)
+  useHideAriaSiblings(snackbarRef, isOpen)
+
+  const testId = rest['data-testid']
+  const handleKeyDown = (e) => {
+    if (['Escape', 'esc'].includes(e.key) || e.keyCode === 27) {
+      onDismiss(false)
+    }
+  }
+
+  return (
+    <Portal id={id}>
+      {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
+      <div
+        role="dialog"
+        aria-modal="true"
+        onKeyDown={handleKeyDown}
+        aria-describedby={ariaDescribedBy && ariaDescribedBy}
+        aria-labelledby={ariaLabelledBy && ariaLabelledBy}
+        aria-hidden={!isOpen}
+        data-testid={testId}
+        ref={snackbarRef}
+      >
+        {isOpen && children}
+      </div>
+    </Portal>
+  )
 }
 
 Snackbar.propTypes = {
-  id: PropTypes.string,
-  children: PropTypes.node,
+  /** Id of port element */
+  id: PropTypes.string.isRequired,
+  /** Id of an element that labels the modal */
+  ariaLabelledBy: PropTypes.string,
+  /** Id of an element that describes the modal */
+  ariaDescribedBy: PropTypes.string,
+  /** The Modal's children */
+  children: PropTypes.node.isRequired,
+  /** Boolean that sets the state of the modal */
+  isOpen: PropTypes.bool,
+  /** handler that onDismiss the state of the modal */
+  onDismiss: PropTypes.func.isRequired,
 }
--- src/components/Snackbar/Snackbar.md
+++ src/components/Snackbar/Snackbar.md
@@ -1,23 +1,53 @@
-```jsx
-import { Icon } from '../../nora/components/Icon/Icon.js'
+Snackbar a11y / UX—some things to try:
 
+- Press `<Tab>` key and note it puts focus on the close button within the Snackbar
+- Press `<Enter>` or `<Space>` key and note it closes the Snackbar
+- Use the "Open Programmatically" button
+- Press `<Escape>` key and note it closes the Snackbar
+
+```jsx
+import React, { useState } from 'react'
+import { Icon } from '../../nora/components/Icon/Icon.js'
+import { Button } from '../index'
 /**
  * All of this is custom code for example's sake (the styles,
  * and JSX children used within the <Snackbar> element)
  */
 import styles from './Snackbar.module.scss'
-const [isOpen, setIsOpen] = React.useState(true)
 
-const klasses = `${styles.SnackbarContainer} ${styles.SnackbarSkin} ${
-  styles.Bottom
-} ${styles.Left} ${isOpen ? styles.Open : ''}`
+const [isOpen, setIsOpen] = useState(true)
+const SNACKBAR_LBL_ID = 'a11y-snackbar-id'
+const SNACKBAR_DESC_ID = 'a11y-described-by'
 
-;<Snackbar id="foo">
-  <div className={klasses}>
-    <p>😞 Something bad happened</p>
-    <button className={styles.CustomButton} onClick={() => setIsOpen(false)}>
-      <Icon iconName="window-close" iconPrefix="fal" />
-    </button>
-  </div>
-</Snackbar>
+const SnackbarExample = () => {
+  const klasses = `${styles.SnackbarContainer} ${styles.SnackbarSkin} ${
+    styles.Bottom
+  } ${styles.Left} ${isOpen ? styles.Open : ''}`
+  return (
+    <>
+      <Button.Medium.Black onClick={() => setIsOpen(true)}>
+        Open Programmatically
+      </Button.Medium.Black>{' '}
+      <Snackbar
+        id="le-snackbar"
+        isOpen={isOpen}
+        onDismiss={setIsOpen}
+        ariaLabelledBy={SNACKBAR_LBL_ID}
+        ariaDescribedBy={SNACKBAR_DESC_ID}
+      >
+        <div className={klasses}>
+          <p>😞 Something bad happened</p>
+          <button
+            tabIndex="0"
+            className={styles.CustomButton}
+            onClick={() => setIsOpen(false)}
+          >
+            <Icon iconName="window-close" iconPrefix="fal" />
+          </button>
+        </div>
+      </Snackbar>
+    </>
+  )
+}
+;<SnackbarExample />
 ```
