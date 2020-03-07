9ffc9e40782be4786582e22223f9fa5b3c53fb73
Rob Levin <rob@getethos.com>
2020-03-07 06:44:18 -0800

    Expose checked prop

--- src/components/index.d.ts
+++ src/components/index.d.ts
@@ -118,6 +118,7 @@ export declare const CheckboxInput: {
     disabled,
     name,
     initialValue,
+    checked,
     currentValue,
     currentError,
     setFieldTouched,
@@ -132,6 +133,7 @@ export declare const CheckboxInput: {
     name: string
     'data-tid': string
     initialValue?: string | boolean
+    checked?: boolean
     currentValue?: string | boolean
     currentError?: string
     setFieldTouched?: (touched: boolean) => void
@@ -1095,6 +1097,7 @@ export declare const NoraCheckboxInput: {
   ({
     name,
     initialValue,
+    checked,
     children,
     validator,
     disabled,
@@ -1102,6 +1105,7 @@ export declare const NoraCheckboxInput: {
   }: {
     name: string
     initialValue?: string | boolean
+    checked?: boolean
     'data-tid': string
     children: React.ReactNode
     disabled?: boolean
