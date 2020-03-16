98806b51cde5e29b7c242749172d40e1ce4904b7
stephanieL96 <12839832+stephaniel96@users.noreply.github.com>
2020-03-16 14:12:55 -0700

    Add test for onClick

--- src/nora/components/IconLink/IconLink.test.js
+++ src/nora/components/IconLink/IconLink.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import { IconLink } from './IconLink'
+import { link } from 'fs'
 
 describe('IconLink', () => {
   it('renders default IconLink', () => {
@@ -18,5 +19,27 @@ describe('IconLink', () => {
       )
       .toJSON()
     expect(tree).toMatchSnapshot()
+  })
+
+  it('calls onClick when provided', () => {
+    const onClickStub = jest.fn()
+    const tree = renderer.create(
+      <IconLink
+        iconPrefix="fas"
+        iconName="check-square"
+        iconClassName="iconClassName"
+        iconContainerClassName="iconContainerClasses"
+        textClassName="linkClassName"
+        textPosition="right"
+        copy="Completed"
+        onClick={onClickStub}
+      />
+    )
+
+    renderer.act(() => {
+      tree.root.props.onClick()
+    })
+
+    expect(onClickStub).toHaveBeenCalledTimes(1)
   })
 })
