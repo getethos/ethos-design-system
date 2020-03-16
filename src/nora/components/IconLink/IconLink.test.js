f141f6ff34223a235f84aa506af4bcf8b6ce3adb
Stephanie Liu <12839832+stephaniel96@users.noreply.github.com>
2020-03-16 14:29:54 -0700

    Make onClick optional in IconLink (#301)

--- src/nora/components/IconLink/IconLink.test.js
+++ src/nora/components/IconLink/IconLink.test.js
@@ -19,4 +19,26 @@ describe('IconLink', () => {
       .toJSON()
     expect(tree).toMatchSnapshot()
   })
+
+  it('calls onClick when provided', () => {
+    const onClickStub = jest.fn()
+    const tree = renderer.create(
+      <IconLink
+        iconPrefix="fas"
+        iconName="check-square"
+        iconClassName="iconClassName"
+        iconContainerClassName="iconContainerClasses"
+        textClassName="linkClassName"
+        textPosition="right"
+        copy="Completed"
+        onClick={onClickStub}
+      />
+    )
+
+    renderer.act(() => {
+      tree.root.props.onClick()
+    })
+
+    expect(onClickStub).toHaveBeenCalledTimes(1)
+  })
 })
