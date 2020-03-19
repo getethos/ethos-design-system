58717bf08501f5b1ac5ac35f684630a91407ffa7
Cole <c7hudson@gmail.com>
2020-03-19 16:28:24 -0700

    update TypeTwo test

--- src/components/TypeTwo/TitleSmallTwo.js
+++ src/components/TypeTwo/TitleSmallTwo.js
@@ -1,39 +1,12 @@
 import { TypeBase, TypeFoundry } from '../TypeBase/TypeBase.js'
 
-// Currently a mirror of Type/TitleSmall for demonstrative purposes
+// Currently drastically different than existing TitleSmall for testing purposes
 export const TitleSmallTwo = {
   Sans: {
     Light300: TypeFoundry({
-      subtype: TypeBase.SUBTYPES.TITLE_SMALL,
-      typeface: TypeBase.TYPEFACES.THEINHARDT,
-      weight: TypeBase.WEIGHTS.LIGHT_300,
-    }),
-    Regular400: TypeFoundry({
-      subtype: TypeBase.SUBTYPES.TITLE_SMALL,
-      typeface: TypeBase.TYPEFACES.THEINHARDT,
-      weight: TypeBase.WEIGHTS.REGULAR_400,
-    }),
-    Medium500: TypeFoundry({
-      subtype: TypeBase.SUBTYPES.TITLE_SMALL,
+      subtype: TypeBase.SUBTYPES.TITLE_MEDIUM,
       typeface: TypeBase.TYPEFACES.THEINHARDT,
       weight: TypeBase.WEIGHTS.MEDIUM_500,
-    }),
-  },
-  Serif: {
-    Regular400: TypeFoundry({
-      subtype: TypeBase.SUBTYPES.TITLE_SMALL,
-      typeface: TypeBase.TYPEFACES.CAMBON,
-      weight: TypeBase.WEIGHTS.REGULAR_400,
-    }),
-    Book500: TypeFoundry({
-      subtype: TypeBase.SUBTYPES.TITLE_SMALL,
-      typeface: TypeBase.TYPEFACES.CAMBON,
-      weight: TypeBase.WEIGHTS.BOOK_500,
-    }),
-    Demi600: TypeFoundry({
-      subtype: TypeBase.SUBTYPES.TITLE_SMALL,
-      typeface: TypeBase.TYPEFACES.CAMBON,
-      weight: TypeBase.WEIGHTS.DEMI_600,
     }),
   },
 }
