3c09c76386c9dbad83078fd9fd7787862e775f32
Cole <c7hudson@gmail.com>
2020-03-20 12:06:30 -0700

    remove unused file

--- src/components/TypeBase/index.js
+++
@@ -1,7 +0,0 @@
-// To preserve react-styleguidist alphabetical ordering, we've moved
-// some of these up to the top-level directory
-export { TitleSmall } from './TitleSmall.js'
-export { TitleMedium } from './TitleMedium.js'
-export { TitleLarge } from './TitleLarge.js'
-export { TitleXLarge } from './TitleXLarge.js'
-export { TitleXXLarge } from './TitleXXLarge.js'
