9fd8fe56f6e734f00073bb27b571b0c3124c6f7f
Rob Levin <rob@getethos.com>
2020-04-03 16:57:53 -0700

    NoraRadioButtonGroup follow up -- missing typings that were preventing the form engine from validating properly.

--- src/nora/components/NoraRadioButtonGroup/NoraRadioButtonGroup.js
+++ src/nora/components/NoraRadioButtonGroup/NoraRadioButtonGroup.js
@@ -3,7 +3,19 @@ import PropTypes from 'prop-types'
 import { RadioButtonGroup } from '../../../components/index'
 import styles from './NoraRadioButtonGroup.module.scss'
 
-export const NoraRadioButtonGroup = ({ name, options, onChange }) => {
+export const NoraRadioButtonGroup = ({
+  name,
+  options,
+  onChange,
+  formChangeHandler,
+  initialValue = undefined,
+  currentValue,
+  currentError,
+  formTouched,
+  disabled,
+  validator,
+  required,
+}) => {
   return (
     <div className={styles.NoraRadioButtonGroup}>
       <RadioButtonGroup
@@ -11,6 +23,14 @@ export const NoraRadioButtonGroup = ({ name, options, onChange }) => {
         labelCopy=""
         options={options}
         onChange={onChange}
+        formChangeHandler={formChangeHandler}
+        initialValue={initialValue}
+        currentValue={currentValue}
+        currentError={currentError}
+        formTouched={formTouched}
+        disabled={disabled}
+        validator={validator}
+        required={required}
       />
     </div>
   )
@@ -20,4 +40,12 @@ NoraRadioButtonGroup.propTypes = {
   name: PropTypes.string.isRequired,
   options: PropTypes.array.isRequired,
   onChange: PropTypes.func,
+  formChangeHandler: PropTypes.func,
+  initialValue: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+  currentValue: PropTypes.string,
+  currentError: PropTypes.string,
+  formTouched: PropTypes.bool,
+  disabled: PropTypes.bool,
+  validator: PropTypes.func,
+  required: PropTypes.bool,
 }
