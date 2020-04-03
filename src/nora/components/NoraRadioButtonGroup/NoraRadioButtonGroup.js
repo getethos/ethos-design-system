5b0f2f4421ca643d27b77db33b976df1f8437f8c
Rob Levin <rob@getethos.com>
2020-04-03 15:09:36 -0700

    Realized we don't need children and changed the interface around a bit. Typings

--- src/nora/components/NoraRadioButtonGroup/NoraRadioButtonGroup.js
+++ src/nora/components/NoraRadioButtonGroup/NoraRadioButtonGroup.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { RadioButtonGroup } from '../../../components/index'
 import styles from './NoraRadioButtonGroup.module.scss'
 
-export const NoraRadioButtonGroup = ({ children, ...props }) => {
+export const NoraRadioButtonGroup = ({ name, options, onChange }) => {
   return (
     <div className={styles.NoraRadioButtonGroup}>
-      <RadioButtonGroup labelCopy="" {...props}>
-        {children}
-      </RadioButtonGroup>
+      <RadioButtonGroup
+        name={name}
+        labelCopy=""
+        options={options}
+        onChange={onChange}
+      />
     </div>
   )
 }
