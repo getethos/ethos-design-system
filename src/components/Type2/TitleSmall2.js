86e642970b722faa230212dde2b5b829dc2f801b
c7hudson <c7hudson@gmail.com>
2020-04-10 15:27:59 -0700

    TitleSmall2 js/md

--- src/components/Type2/TitleSmall2.js
+++ src/components/Type2/TitleSmall2.js
@@ -1,12 +1,28 @@
 import { TypeBase, TypeFoundry } from '../TypeBase/TypeBase.js'
 
-// Currently drastically different than existing TitleSmall for testing purposes
 export const TitleSmall2 = {
   Sans: {
-    Light300: TypeFoundry({
-      subtype: TypeBase.SUBTYPES.TITLE_MEDIUM,
+    Medium500: TypeFoundry({
+      subtype: TypeBase.SUBTYPES.TITLE_SMALL2,
       typeface: TypeBase.TYPEFACES.THEINHARDT,
       weight: TypeBase.WEIGHTS.MEDIUM_500,
+    }),
+    Regular400: TypeFoundry({
+      subtype: TypeBase.SUBTYPES.TITLE_SMALL2,
+      typeface: TypeBase.TYPEFACES.THEINHARDT,
+      weight: TypeBase.WEIGHTS.REGULAR_400,
+    }),
+  },
+  Serif: {
+    Medium500: TypeFoundry({
+      subtype: TypeBase.SUBTYPES.TITLE_SMALL2,
+      typeface: TypeBase.TYPEFACES.CAMBON,
+      weight: TypeBase.WEIGHTS.MEDIUM_500,
+    }),
+    Book500: TypeFoundry({
+      subtype: TypeBase.SUBTYPES.TITLE_SMALL2,
+      typeface: TypeBase.TYPEFACES.CAMBON,
+      weight: TypeBase.WEIGHTS.BOOK_500,
     }),
   },
 }
