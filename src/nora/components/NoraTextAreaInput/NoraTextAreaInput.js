162061bf0fd2a66cd0a2a25c58efdc8162db5c50
Rob Levin <rob@getethos.com>
2020-04-10 12:48:46 -0700

    connects the nora textarea to the form engine

--- src/nora/components/NoraTextAreaInput/NoraTextAreaInput.js
+++ src/nora/components/NoraTextAreaInput/NoraTextAreaInput.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-
+import useErrorMessage from '../../../hooks/useErrorMessage.js'
+import useInputValidation from '../../../hooks/useInputValidation.js'
 import { BaseTextAreaInput } from '../../../components/TextAreaInput/BaseTextAreaInput'
 import styles from './NoraTextAreaInput.module.scss'
 
@@ -17,7 +18,70 @@ export const NoraTextAreaInput = ({
   onChange,
   ...rest
 }) => {
+  const {
+    formChangeHandler,
+    validator,
+    initialValue,
+    currentValue,
+    currentError,
+    setFieldTouched,
+  } = { ...rest }
+
   const classes = [styles.NoraTextInput, textClassName].join(' ')
+
+  // Set up validation hooks
+  const [getError, setError, , validate] = useErrorMessage(validator)
+
+  const [val, setVal] = useState(currentValue || initialValue || '')
+
+  const [touched, setTouched] = useState(initialValue ? true : false)
+
+  const [doValidation] = useInputValidation({
+    validate,
+    setError,
+    formChangeHandler,
+  })
+
+  const handleChange = (ev) => {
+    const val = ev.target.value
+    setVal(val)
+
+    // We call setTouched in onBlur, so can reliably call getter here
+    doValidation(val, touched)
+
+    if (!!onChange) onChange(ev)
+  }
+
+  const setAllTouched = () => {
+    // We set touched to change the react state, but it's async and
+    // processing still, so, we use a flag for doValidation
+    setTouched(true)
+    // Also tell the form we've been touched
+    if (setFieldTouched) {
+      setFieldTouched(true)
+    }
+  }
+
+  // Initial Value aka prefilled—are considered "touched", but must prevalidate
+  // which will in turn update the internal form state as to their validity
+  useEffect(() => {
+    if (!!formChangeHandler && initialValue) {
+      console.log('TextAreaInput useEffect -- calling doValidation')
+      doValidation(initialValue, '')
+    }
+  }, [])
+
+  const handleBlur = (ev) => {
+    setAllTouched()
+    doValidation(ev.target.value, true)
+    if (!!onBlur) onBlur(ev)
+  }
+
+  // TODO: this indicates a field level error on the NoraTextAreaInput
+  if (getError(currentError, touched)) {
+    console.log('NoraTextAreaInput field error: ', currentError)
+  }
+
   return (
     <>
       {labelCopy && <label className={labelClassName}>{labelCopy}</label>}
@@ -26,10 +90,10 @@ export const NoraTextAreaInput = ({
         disabled={disabled}
         name={name}
         placeholder={placeholder}
-        onBlur={onBlur}
+        onBlur={handleBlur}
         onFocus={onFocus}
-        onChange={onChange}
-        value={value}
+        onChange={handleChange}
+        value={val}
         data-tid={rest['data-tid']}
       />
     </>
