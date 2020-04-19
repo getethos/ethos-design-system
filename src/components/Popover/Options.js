f52a0879865b4b3876f677f866e4cdd9f11200c9
Rob Levin <rob@getethos.com>
2020-04-19 12:03:43 -0700

    Improve the docs for activeOption, selectedOption and others.

--- src/components/Popover/Options.js
+++ src/components/Popover/Options.js
@@ -6,14 +6,18 @@ import uuidv4 from 'uuid/v4'
 
 /**
  * @param {object} props -- the props
- * @param {int} activeOption - the currently active option item
+ * @param {int} activeOption - the currently active option item.
+ * This is the item that is currently being focused on (but not
+ * necessarily selected yet!)
  * @param {string} dataKey - the key that will be used to access the
  * items within the entities list which is also passed in
  * @param {array} entities - an array of object that have the
  * `dataKey` which is used to access the items individually
  * @param {func} onChange - callback for on change
  * @param {array} optionsRefs- an array of option refs
- * @param {int} selectedOption - the currently selected option item
+ * @param {int} selectedOption - the currently selected option item.
+ * Unlike an active item, this signifies that the user has actually selected
+ * or chosen this item. Of course an item can be both active and selected.
  * @param {func} setSelectedAndActiveOptions - callback that gets called
  * once an option is selected
  */
