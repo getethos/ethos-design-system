6b86bd99067760095ef836f6affbed9e89e7677c
Rob Levin <rob@getethos.com>
2020-04-27 22:42:59 -0700

    NoraNoraButtonInput

--- src/nora/components/NoraButtonInput/NoraButtonInput.js
+++ src/nora/components/NoraButtonInput/NoraButtonInput.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { NoraTextInput } from '../NoraTextInput'
+import { Icon } from '../../../components/index'
+import styles from './NoraButtonInput.module.scss'
 
 /**
  * NoraButtonInput
@@ -9,5 +11,50 @@ import { NoraTextInput } from '../NoraTextInput'
  * @return {JSX.Element}
  */
 export const NoraButtonInput = (props) => {
-  return <NoraTextInput {...props} />
+  const cloned = { ...props }
+  const iconPrefix = cloned.iconPrefix
+  const iconName = cloned.iconName
+  // Side refers to side the button is on (so input is opposite)
+  const side = cloned.side
+  const buttonDisabled = cloned.buttonDisabled
+  // Remove these so EDS TextInput et al don't throw error
+  delete cloned.iconPrefix
+  delete cloned.iconName
+  delete cloned.side
+  delete cloned.buttonDisabled
+
+  const getButtonStyles = () => {
+    let classes = ''
+    if (side === 'right') {
+      classes = styles.ButtonRight
+    } else {
+      classes = styles.ButtonLeft
+    }
+
+    if (buttonDisabled) {
+      classes = [classes, styles.Disabled].join(' ')
+    }
+    return classes
+  }
+
+  return (
+    <div className={styles.NoraButtonInputContainer}>
+      {side === 'right' && (
+        <NoraTextInput className={styles.InputLeft} {...cloned} />
+      )}
+      <button
+        className={getButtonStyles()}
+        tabIndex="-1"
+        onClick={() => {
+          console.log('got clicked...')
+        }}
+      >
+        <Icon iconName={iconName} iconPrefix={iconPrefix} />
+      </button>
+
+      {side === 'left' && (
+        <NoraTextInput className={styles.InputRight} {...cloned} />
+      )}
+    </div>
+  )
 }
--- src/nora/components/NoraButtonInput/NoraButtonInput.md
+++ src/nora/components/NoraButtonInput/NoraButtonInput.md
@@ -8,6 +8,10 @@ const formChangeHandlerStub = () => {}
   name="example"
   data-tid="the-button-input"
   formChangeHandler={formChangeHandlerStub}
+  buttonDisabled
+  iconName="trash-alt"
+  iconPrefix="far"
+  side="right"
   validator={(x) => {
     const truthyErr = validateExists(x)
     if (!!truthyErr) return truthyErr
@@ -15,5 +19,29 @@ const formChangeHandlerStub = () => {}
     if (!!minMaxErr) return minMaxErr
     return x.length % 2 ? 'Text does not have an even number of characters' : ''
   }}
-/>
+></NoraButtonInput>
+```
+
+```jsx
+import validateMinMaxFactory from '../../../validators/validateMinMax'
+import validateExists from '../../../validators/validateExists'
+// formChangeHandler gets wired up automatically if using <Form /> component
+const formChangeHandlerStub = () => {}
+
+;<NoraButtonInput
+  name="example"
+  data-tid="the-button-input"
+  formChangeHandler={formChangeHandlerStub}
+  iconName="trash-alt"
+  buttonDisabled
+  iconPrefix="far"
+  side="left"
+  validator={(x) => {
+    const truthyErr = validateExists(x)
+    if (!!truthyErr) return truthyErr
+    const minMaxErr = validateMinMaxFactory(5, 20)(x)
+    if (!!minMaxErr) return minMaxErr
+    return x.length % 2 ? 'Text does not have an even number of characters' : ''
+  }}
+></NoraButtonInput>
 ```
