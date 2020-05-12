0acc1a571d544ac70a75771cc9ca931c62e88ed3
Rob Levin <rob@getethos.com>
2020-05-12 12:22:55 -0700

    Update Select tests to use testing-libray instead -- missed a spec.

--- src/components/Select/Select.test.js
+++ src/components/Select/Select.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Select } from './Select.js'
-import { render } from '@testing-library/react'
+import { render, fireEvent, waitForElement } from '@testing-library/react'
 
 describe('Select', () => {
   const mockProps = {
@@ -9,15 +9,13 @@ describe('Select', () => {
     'data-tid': 'my-select',
   }
 
-  test.only('matches snapshot', () => {
+  test('matches snapshot', () => {
     const tree = render(<Select {...mockProps} />)
     expect(tree).toMatchSnapshot()
   })
 
   test('adding a title prop adds a div and aria-label', () => {
-    const tree = renderer
-      .create(<Select {...mockProps} title="Hello World" />)
-      .toJSON()
+    const tree = render(<Select {...mockProps} title="Hello World" />)
     expect(tree).toMatchSnapshot()
   })
 })
