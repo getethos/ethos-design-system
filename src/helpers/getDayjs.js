10479934cee0f77cc29dd6ce5bf2c50db3e4e52b
Rob Levin <rob@getethos.com>
2020-05-18 18:06:39 -0700

    Extends dayjs with the utc and customParseFormat plugins

--- src/helpers/getDayjs.js
+++ src/helpers/getDayjs.js
@@ -1,4 +1,12 @@
 // https://github.com/iamkun/dayjs/issues/480
 import * as dayjs_ from 'dayjs'
 const dayjs = dayjs_.default || dayjs_
+
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+dayjs.extend(customParseFormat)
+
+// Adds the UTC plugin
+import dayjsPluginUTC from 'dayjs-plugin-utc'
+dayjs.extend(dayjsPluginUTC)
+
 export default dayjs
