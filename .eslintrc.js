e8b3749f10898795772fefc38d8b30b1bf24ed35
Ethan Tran <ethandt92@gmail.com>
2020-10-24 00:01:41 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -74,6 +74,8 @@ module.exports = {
       plugins: ['@typescript-eslint'],
       rules: {
         'react/prop-types': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/ban-types': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-empty-function': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
