54e8b2ad04092c89718fa58d6b818fcb4e1e73b5
Ethan Tran <ethandt92@gmail.com>
2020-10-23 23:56:41 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -74,6 +74,10 @@ module.exports = {
       plugins: ['@typescript-eslint'],
       rules: {
         'react/prop-types': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
       },
     },
   ],
