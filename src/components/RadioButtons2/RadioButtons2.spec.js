b9cc58de6f5bba2b06a7fbaf6f48d842765980d3
Victor Li <victorli5674@yahoo.com>
2020-11-03 17:44:27 -0800

    radio button 2

---
+++ src/components/RadioButtons2/RadioButtons2.spec.js
@@ -0,0 +1,109 @@
+import React from 'react'
+import { RadioButtonGroup2, focusHelper } from './RadioButtons2.js'
+import renderer from 'react-test-renderer'
+
+var focusOnTargetSpy = jest.fn()
+jest.spyOn(focusHelper, 'focus').mockImplementation(focusOnTargetSpy)
+
+describe('RadioButtonGroup2', () => {
+  describe('API', () => {
+    test('exports properly', () => {
+      expect(RadioButtonGroup2).toBeDefined()
+    })
+
+    test('sets `initialValue` and `onChange` handler callback fires', () => {
+      const onChangeStub = jest.fn()
+      const optionValues = ['foo', 'bar']
+
+      const optionsRadios = optionValues.map((t) => ({
+        name: t,
+        value: t,
+        label: `${t} -- description`,
+      }))
+
+      const tree = renderer.create(
+        <RadioButtonGroup2
+          name="intent"
+          labelCopy="When would you like to apply?"
+          initialValue={optionValues[0]}
+          onChange={onChangeStub}
+          options={optionsRadios}
+        />
+      )
+
+      const instance = tree.root
+      // const options = instance.findAllByType()
+      const options = instance.findAll(
+        (el) => el.type == 'input' && el.props.type == 'radio'
+      )
+
+      const [first] = options
+
+      renderer.act(() => {
+        first.props.onClick()
+      })
+
+      expect(onChangeStub).toHaveBeenCalledWith({
+        value: optionValues[0],
+        isAnswered: true,
+      })
+    })
+
+    test('click handler fires', () => {
+      const onClickStub = jest.fn()
+      const value = 'foo'
+      const tree = renderer.create(
+        <RadioButtonGroup2
+          name="intent"
+          labelCopy="When would you like to apply?"
+          options={[
+            {
+              label: value,
+              onClick: onClickStub,
+              name: value,
+              value: value,
+            },
+          ]}
+        />
+      )
+      const instance = tree.root
+      const option = instance.find(
+        (el) => el.type == 'input' && el.props.type == 'radio'
+      )
+      renderer.act(() => {
+        option.props.onClick()
+      })
+      expect(onClickStub).toHaveBeenCalled()
+    })
+  })
+
+  describe('default rendering', () => {
+    test('RadioButtonGroup2', () => {
+      const tree = renderer
+        .create(
+          <RadioButtonGroup2
+            name="gender"
+            initialValue="M"
+            labelCopy="When would you like to apply?"
+            onChange={() => {}}
+            options={[
+              {
+                'data-tid': 'gender-male',
+                name: 'male',
+                label: 'Male',
+                value: 'M',
+              },
+              {
+                'data-tid': 'gender-female',
+                name: 'female',
+                label: 'Female',
+                value: 'F',
+              },
+            ]}
+          />
+        )
+        .toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+  })
+})
