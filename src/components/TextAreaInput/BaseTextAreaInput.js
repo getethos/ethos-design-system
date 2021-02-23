d9200a6780ff2d93d68e71bba11b84fdc6d24042
Andrew Bright <anbright96@gmail.com>
2021-02-22 19:38:30 -0500

    Add rows to textarea (#379)
    * Add rows property to text area
    
    * 1.1.239

--- src/components/TextAreaInput/BaseTextAreaInput.js
+++ src/components/TextAreaInput/BaseTextAreaInput.js
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types'
  * Required props:
  * @param {string} name
  * @param {string} 'data-tid'
+ * @param {number} rows the native text area rows attribute (https://www.w3schools.com/tags/att_textarea_rows.asp)
  */
 
 export const BaseTextAreaInput = ({
@@ -19,6 +20,7 @@ export const BaseTextAreaInput = ({
   onFocus,
   onChange,
   value,
+  rows,
   ...rest
 }) => {
   return (
@@ -32,6 +34,7 @@ export const BaseTextAreaInput = ({
       onBlur={onBlur}
       onFocus={onFocus}
       value={value}
+      rows={rows}
       data-tid={rest['data-tid']}
     />
   )
@@ -41,6 +44,7 @@ BaseTextAreaInput.propTypes = {
   className: PropTypes.string,
   disabled: PropTypes.bool,
   name: PropTypes.string.isRequired,
+  rows: PropTypes.number,
   placeholder: PropTypes.string,
   onPaste: PropTypes.func,
   onBlur: PropTypes.func,
