2f90972b06fa0b8306081292e9fd9831e8fb0939
Victor Li <victorli5674@yahoo.com>
2021-08-30 23:28:14 -0700

    make disabled prop for zip input possible

--- src/components/ZipInput/ZipInput.js
+++ src/components/ZipInput/ZipInput.js
@@ -22,6 +22,7 @@ export const ZipInput = (props) => {
     guide = true,
     keepCharPositions = true,
     autoComplete,
+    disabled = false,
     ...restProps
   } = props
 
@@ -89,6 +90,7 @@ export const ZipInput = (props) => {
         getTouched={touched}
         setTouched={setTouched}
         autoComplete={autoComplete}
+        disabled={disabled}
       />
       {getError(currentError, touched)}
     </>
