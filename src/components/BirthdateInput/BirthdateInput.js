1bd58daa1a5945196122ea118c725fc966ca3363
wesleydrobinson <wesley@getethos.com>
2021-09-01 10:55:56 -0700

    tighten BirthdateInput; pass thru labelColor,labelWeight,labelClasses;

--- src/components/BirthdateInput/BirthdateInput.js
+++ src/components/BirthdateInput/BirthdateInput.js
@@ -2,52 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { DateInput } from '../DateInput'
 import * as Validators from '../../validators/BirthdateInputValidator'
-const { DATE_FORMATS } = Validators
 
-export const BirthdateInput = (props) => {
-  const {
-    optional,
-    dateFormat,
-    allCaps,
-    capitalize,
-    labelCopy,
-    validator,
-    formChangeHandler,
-    initialValue,
-    currentValue,
-    currentError,
-    formTouched,
-    setFieldTouched,
-    autoComplete,
-    classOverrides,
-    ...restProps
-  } = props
-  return (
-    <DateInput
-      optional={optional}
-      dateFormat={dateFormat}
-      allCaps={allCaps}
-      capitalize={capitalize}
-      labelCopy={labelCopy}
-      validator={validator}
-      formChangeHandler={formChangeHandler}
-      initialValue={initialValue}
-      currentValue={currentValue}
-      currentError={currentError}
-      formTouched={formTouched}
-      setFieldTouched={setFieldTouched}
-      autoComplete={autoComplete}
-      classOverrides={classOverrides}
-      {...restProps}
-    />
-  )
-}
+export const BirthdateInput = (props) => <DateInput {...props} />
 
 BirthdateInput.propTypes = {
   /** whether this field is optional or not */
   optional: PropTypes.bool,
   /** Date format -- one of: 'mm/dd/yyyy', 'mm/yyyy', 'mm/yy' */
-  dateFormat: PropTypes.oneOf(DATE_FORMATS),
+  dateFormat: PropTypes.oneOf(Validators.DATE_FORMATS),
   /** Required data-tid used as a unique id for targeting test selectors */
   'data-tid': PropTypes.string,
   /** whether disabled or not */
@@ -60,6 +22,9 @@ BirthdateInput.propTypes = {
   name: PropTypes.string.isRequired,
   /** label  */
   labelCopy: PropTypes.string,
+  labelColor: PropTypes.string,
+  labelWeight: PropTypes.string,
+  labelClasses: PropTypes.string,
   /** field validation callback */
   validator: PropTypes.func,
   /** Optionally sets a default value for the birthdate */
