ee33a401c1f0543918105f069d8199157eaff1e2
coleEthos <87672141+coleEthos@users.noreply.github.com>
2021-09-15 14:15:31 -0700

    Button refreshIconInner class (#410)
    * refreshIconInner class
    
    * v1.2.8
    
    * refreshIcon type decleration

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -152,7 +152,7 @@ function RefreshIcon() {
       viewBox="0 1 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className={styles.refreshIcon}
+      className={styles.refreshIconInner}
     >
       <g clipPath="url(#clip0)">
         <path
