2e82cd4d8c1474c385b9a97b34efa7e12e89ef44
Cole Hudson <cole.hudson@getethos.com>
2021-11-16 10:47:55 -0800

    remove old comment

--- src/components/UniversalNavbarExpanded/SampleContent.js
+++ src/components/UniversalNavbarExpanded/SampleContent.js
@@ -20,9 +20,6 @@ const playIcon = () => (
  * Sample of UniversalNavbarExpanded links.
  * For demonstration in styleguide & test files (.md/.test.js files) only!
  * Real implementation of this structure should use uuid() for id values!
- *
- * TODO when Kustomer implementation is ready and we want to include a talk button
- *      add TALK to the object with href & title
  */
 export const SINGLE_CTA = {
   href: 'https://stage.ethoslife.com/agents-portal/',
