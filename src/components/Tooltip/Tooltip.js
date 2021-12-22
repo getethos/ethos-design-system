84e2069314129f217d71d0ac01955c00eaa655be
Farhan Kaskar <farhan.kaskar@getethos.com>
2021-12-22 16:55:49 -0500

    minor fixes

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -134,7 +134,7 @@ export const Tooltip = ({
                 visible={tooltipVisible}
                 details={details}
                 popperBoxStyles={popperBoxStyles}
-                useSoftEdges
+                useSoftEdges={useSoftEdges}
                 {...rest}
               />
             )}
