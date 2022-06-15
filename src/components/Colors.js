a60446cc95568de9629168591586cdcfa92e9207
James Hubert <james.hubert@getethos.com>
2022-06-15 11:19:49 -0700

    First swing at making all colors available in TypeBase

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -2,14 +2,27 @@
 // This map is for high-level colors names (conflating e.g. opaque/transparent).
 // It's also possible we'll need to make this isomorphic with the Sass file.
 export const COLORS = {
+  // TODO: Confirm that the value strings are what they need to be; should they match their css variables in Colors.scss?
   // Brand
+  BRAND_BUTTERCUP: 'BrandButtercup',
+  BRAND_DUCKEGG: 'BrandDuckegg',
   BRAND_FOREST: 'BrandForest',
+  BRAND_MOSS: 'BrandMoss',
+  BRAND_MOSS_TRANSLUSCENT: 'BrandMossTranslucent',
   BRAND_SALAMANDER: 'BrandSalamander',
+  BRAND_SAND: 'BrandSand',
 
   // Grayscales
+  GRAY_DARK_HOVER: 'GrayDarkHover',
+  GRAY_DARK_HOVER_TRANS: 'GrayDarkHoverTrans',
+  GRAY_LIGHT_HOVER: 'GrayLightHover',
+  GRAY_LIGHT_HOVER_TRANS: 'GrayLightHoverTrans',
   GRAY_PRIMARY: 'GrayPrimary',
+  GRAY_PRIMARY_TRANS: 'GrayPrimaryTrans',
   GRAY_SECONDARY: 'GraySecondary',
+  GRAY_SECONDARY_TRANS: 'GraySecondaryTrans',
   GRAY_STROKE_AND_DISABLED: 'GrayStrokeAndDisabled',
+  GRAY_STROKE_AND_DISABLED_TRANS: 'GrayStrokeAndDisabled',
   WHITE: 'White',
 
   // Miscellany
