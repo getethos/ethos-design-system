4b8e6928da19fa4c7fd5607937f530e36892afb2
James Hubert <james.hubert@getethos.com>
2022-06-16 10:53:46 -0700

    Make all colors available in TypeBase.scss

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -1,6 +1,5 @@
 // The colors and vars in Colors.scss are baked, but this JS interface is not.
 // This map is for high-level colors names (conflating e.g. opaque/transparent).
-// It's also possible we'll need to make this isomorphic with the Sass file.
 export const COLORS = {
   // Brand
   BRAND_BUTTERCUP: 'BrandButtercup',
@@ -21,8 +20,6 @@ export const COLORS = {
   GRAY_SECONDARY: 'GraySecondary',
   GRAY_SECONDARY_TRANS: 'GraySecondaryTrans',
   GRAY_STROKE_AND_DISABLED: 'GrayStrokeAndDisabled',
-  GRAY_STROKE_AND_DISABLED_TRANS: 'GrayStrokeAndDisabled',
+  GRAY_STROKE_AND_DISABLED_TRANS: 'GrayStrokeAndDisabledTrans',
   WHITE: 'White',
-
-  // Miscellany
 }
