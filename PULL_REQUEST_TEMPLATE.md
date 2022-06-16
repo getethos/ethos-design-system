ca7f565f283c631be15680005192c4e6373bbd75
James Hubert <jwhubert91@gmail.com>
2022-06-16 12:59:33 -0700

    mxJames / Make all EDS colors available in TypeBase (#447)
    * First swing at making all colors available in TypeBase
    
    * Why is adding a color prop with BrandMossTranslucent not working for Body2?
    
    * Make all colors available in TypeBase.scss
    
    * Add a grayscale example to md file
    
    * v1.2.26
    
    * v1.2.27
    
    * Not all heros wear capes

--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -2,7 +2,7 @@
 
 ### Please review these reminders and either check the box or explain why not:
 
-- [ ] Read and followed the [contributing guidelines](https://eds.ethoslabs.io/#/Guidelines?id=section-contribute)?
+- [ ] Read and followed the [contributing guidelines](https://eds.eks.dev.ethos-int.com/#/Guidelines?id=section-contribute)?
 - [ ] Component is exported from [index.js](https://github.com/getethos/ethos-design-system/blob/master/src/components/index.js)?
 - [ ] Type is exported from [index.d.ts](https://github.com/getethos/ethos-design-system/blob/master/src/components/index.d.ts) so Typescript users can consume it? Added a test and ran `yarn test:types`?
 - [ ] Bumped the yarn version?
--- src/components/Colors.js
+++ src/components/Colors.js
@@ -1,16 +1,25 @@
 // The colors and vars in Colors.scss are baked, but this JS interface is not.
 // This map is for high-level colors names (conflating e.g. opaque/transparent).
-// It's also possible we'll need to make this isomorphic with the Sass file.
 export const COLORS = {
   // Brand
+  BRAND_BUTTERCUP: 'BrandButtercup',
+  BRAND_DUCKEGG: 'BrandDuckegg',
   BRAND_FOREST: 'BrandForest',
+  BRAND_MOSS: 'BrandMoss',
+  BRAND_MOSS_TRANSLUSCENT: 'BrandMossTranslucent',
   BRAND_SALAMANDER: 'BrandSalamander',
+  BRAND_SAND: 'BrandSand',
 
   // Grayscales
+  GRAY_DARK_HOVER: 'GrayDarkHover',
+  GRAY_DARK_HOVER_TRANS: 'GrayDarkHoverTrans',
+  GRAY_LIGHT_HOVER: 'GrayLightHover',
+  GRAY_LIGHT_HOVER_TRANS: 'GrayLightHoverTrans',
   GRAY_PRIMARY: 'GrayPrimary',
+  GRAY_PRIMARY_TRANS: 'GrayPrimaryTrans',
   GRAY_SECONDARY: 'GraySecondary',
+  GRAY_SECONDARY_TRANS: 'GraySecondaryTrans',
   GRAY_STROKE_AND_DISABLED: 'GrayStrokeAndDisabled',
+  GRAY_STROKE_AND_DISABLED_TRANS: 'GrayStrokeAndDisabledTrans',
   WHITE: 'White',
-
-  // Miscellany
 }
