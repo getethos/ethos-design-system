e8f4c85dce7ad13805f53294c99b5607536cc864
Sanskar <Sanskar.Yerawar@getethos.com>
2025-11-25 14:25:14 +0530

    [CEQ-4665] feat: add trackLoginCtaClick to UniversalNavBar (#551)
    * initial commit
    
    * add trackLoginCtaClick on AccountIcon
    
    * fixes
    
    * update version from to 1.3.41

--- src/components/UniversalNavbar/UniversalNavbar.d.ts
+++ src/components/UniversalNavbar/UniversalNavbar.d.ts
@@ -13,8 +13,10 @@ type UniversalNavbarProps = {
   logoHref?: string
   /** State if use is logged in */
   isLoggedIn?: boolean
+  
   /** render CTA node button in the Outer CTA div, prioritized over singleCta */
   renderCtaButton?: (props: { isMobile: boolean }) => React.ReactNode
+
   /** Analytics function run when Login CTA Button is clicked */
   trackLoginCtaClick?: () => void
 }
